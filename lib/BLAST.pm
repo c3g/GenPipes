dcabd4bf15b800888079f099413e3590aba0a927
David Morais <moraisd2@ip03.m>
2013-01-24 12:07:49 -0500

    Fixed blast db error name

--- lib/BLAST.pm
+++ lib/BLAST.pm
@@ -63,7 +63,7 @@ use warnings;
 use Data::Dumper;
 use Config::Simple;
 use LoadConfig;
-
+use File::Basename;
 #-------------------
 # SUB
 #-------------------
@@ -92,9 +92,9 @@ sub align {
     my $laneDirectory = "assembly/" . $sampleName . "/";
 
     $command .= 'module add mugqic/BLAST/2.2.26+;';
-    $command .= ' mkdir -p ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . ';';
+    $command .= ' mkdir -p ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'}) . ';';
     $command .= ' ' . $rH_cfg->{'blast.program'} . ' -num_threads ' . $rH_cfg->{'blast.nbThreads'};
-    $command .= ' -query ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/' . $fileFasta;
+    $command .= ' -query ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/' . $fileFasta;
     $command .= ' -db ' . $rH_cfg->{'blast.db'} . ' -out ' . $laneDirectory . 'fasta_split/' . $outFile . '_BLASTOUT.txt';
     $command .= ' ' . $rH_cfg->{'blast.options'};
 
@@ -115,7 +115,7 @@ sub alignParallel {
     # In this case $db should be passed as an argument
     #------------------------------------------------------------------
     $rH_cfg->{'blast.db'} = defined($db) ? $db : $rH_cfg->{'blast.db'};
-
+	
     my $outFile = $fileFasta;
     $outFile =~ s/\*_//;
     my $command = '';
@@ -123,9 +123,9 @@ sub alignParallel {
     my $laneDirectory = "assembly/" . $sampleName . "/";
 
     $command .= 'module add mugqic/BLAST/2.2.26+;';
-    $command .= ' mkdir -p ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . ';';
+    $command .= ' mkdir -p ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'}) . ';';
     $command .= ' ' . $rH_cfg->{'blast.parallelBlast'} . ' --file ' . $laneDirectory . 'fasta_split/' . $fileFasta;
-    $command .= ' --OUT ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/' . $outFile . '_BLASTOUT.txt';
+    $command .= ' --OUT ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/' . $outFile . '_BLASTOUT.txt';
     $command .= ' -n ' . $rH_cfg->{'blast.nbThreads'} . ' --BLAST ' . '\'' . $rH_cfg->{'blast.program'};
     $command .= ' -db ' . $rH_cfg->{'blast.db'} . ' ' . $rH_cfg->{'blast.options'} . '\'';
 
@@ -149,12 +149,12 @@ sub bestHit {
     my %retVal;
 
     my $laneDirectory = "assembly/" . $sampleName . "/";
-    $command .= 'cat ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/' . '*.txt ';
-    $command .= ' >' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/blastRes.txt ;';
-    $command .= ' sh ' . $rH_cfg->{'blast.blastHq'} . ' ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/blastRes.txt ';
-    $command .= ' >' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/blastRes_HQ.txt ;';
-    $command .= ' ' . $rH_cfg->{'blast.BestHit'} . ' ' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/blastRes_HQ.txt ';
-    $command .= ' >' . $laneDirectory . 'fasta_split/' . $rH_cfg->{'blast.db'} . '/blast_BestHit.txt ;';
+    $command .= 'cat ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/' . '*.txt ';
+    $command .= ' >' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/blastRes.txt ;';
+    $command .= ' sh ' . $rH_cfg->{'blast.blastHq'} . ' ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/blastRes.txt ';
+    $command .= ' >' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/blastRes_HQ.txt ;';
+    $command .= ' ' . $rH_cfg->{'blast.BestHit'} . ' ' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/blastRes_HQ.txt ';
+    $command .= ' >' . $laneDirectory . 'fasta_split/' . basename($rH_cfg->{'blast.db'})  . '/blast_BestHit.txt ;';
 
     $retVal{'command'} = $command;
     return ( \%retVal );
