d67ceddea5a1d97c892cc40b7d8ccaf39d51403c
lletourn <louis.letourneau@mail.mcgill.ca>
2013-02-03 16:50:56 -0500

    Added DNAC support

--- pipelines/dnaseq/pairedVariants.pl
+++ pipelines/dnaseq/pairedVariants.pl
@@ -36,14 +36,14 @@ use strict;
 use warnings;
 #---------------------
 
-BEGIN{
-    #Makesure we can find the GetConfig::LoadModules module relative to this script install
-    use File::Basename;
-    use Cwd 'abs_path';
-    my ( undef, $mod_path, undef ) = fileparse( abs_path(__FILE__) );
-    unshift @INC, $mod_path."lib";
-
-}
+#BEGIN{
+#    #Makesure we can find the GetConfig::LoadModules module relative to this script install
+#    use File::Basename;
+#    use Cwd 'abs_path';
+#    my ( undef, $mod_path, undef ) = fileparse( abs_path(__FILE__) );
+#    unshift @INC, $mod_path."lib";
+#
+#}
 
 
 # Dependencies
@@ -51,10 +51,12 @@ BEGIN{
 use Getopt::Std;
 
 use LoadConfig;
+use BAMtools;
 use SampleSheet;
 use SAMtools;
 use SequenceDictionaryParser;
 use SubmitToCluster;
+use SVtools;
 #--------------------
 
 
@@ -205,4 +207,48 @@ sub mergeFilterBCF {
   return $mergeJobId;
 }
 
+sub DNAC {
+  my $depends = shift;
+  my $rH_cfg = shift;
+  my $rH_samplePair = shift;
+  my $rAoH_seqDictionary = shift;
+
+  my $jobDependency = undef;
+
+  my $sampleName = $rH_samplePair->{'sample'};
+  my $normalBam = $rH_samplePair->{'normal'}.'/'.$rH_samplePair->{'normal'}.'.sorted.dup.bam';
+  my $tumorBam = $rH_samplePair->{'tumor'}.'/'.$rH_samplePair->{'tumor'}.'.sorted.dup.bam';
+  my $outputDir = LoadConfig::getParam($rH_cfg, "DNAC", 'sampleOutputRoot') . $sampleName.'/DNAC/';
+  
+  print 'mkdir -p '.$outputDir."\n";
+  print "DNAC_JOB_IDS=\"\"\n";
+  
+  my $DNAC500File = $outputDir.$sampleName.'.DNAC_500';
+  my $DNAC1000File = $outputDir.$sampleName.'.DNAC_1000.';
+  my $DNAC30000File = $outputDir.$sampleName.'.DNAC_30000';
+  my $bin500File = $DNAC500File.'.bins.tsv';
+  my $bin1000File = $DNAC1000File.'.bins.tsv';
+  my $bin30000File = $DNAC30000File.'.bins.tsv';
+
+  my $command500 = BAMtools::countBins($rH_cfg, $sampleName, $tumorBam, 500, 'chr', $bin500File, $normalBam);
+  my $command1000 = BAMtools::countBins($rH_cfg, $sampleName, $tumorBam, 1000, 'chr', $bin1000File, $normalBam);
+  my $command30000 = BAMtools::countBins($rH_cfg, $sampleName, $tumorBam, 30000, 'genome', $bin30000File, $normalBam);
+
+  $command500 .= ' && '.SVtools::runPairedDNAC($rH_cfg, $sampleName, $bin500File, $DNAC500File, 500);
+  $command1000 .= ' && '.SVtools::runPairedDNAC($rH_cfg, $sampleName, $bin1000File, $DNAC1000File, 1000);
+  $command30000 .= ' && '.SVtools::runPairedDNAC($rH_cfg, $sampleName, $bin30000File, $DNAC30000File, 30000);
+
+  $command500 .= ' && '.SVtools::filterDNAC($rH_cfg, $sampleName, $DNAC500File, $DNAC500File.'.bed', 1);
+  $command1000 .= ' && '.SVtools::filterDNAC($rH_cfg, $sampleName, $DNAC1000File, $DNAC1000File.'.bed', 1);
+  $command30000 .= ' && '.SVtools::filterDNAC($rH_cfg, $sampleName, $DNAC30000File, $DNAC30000File.'.bed', 2);
+
+  my $dnac500JobId = SubmitToCluster::printSubmitCmd($rH_cfg, "DNAC", undef, 'DNAC_500', $jobDependency, $sampleName, $command500);
+  print 'DNAC_JOB_IDS=${DNAC_JOB_IDS}'.LoadConfig::getParam($rH_cfg, 'default', 'clusterDependencySep').$dnac500JobId."\n";
+  my $dnac1000JobId = SubmitToCluster::printSubmitCmd($rH_cfg, "DNAC", undef, 'DNAC_1000', $jobDependency, $sampleName, $command1000);
+  print 'DNAC_JOB_IDS=${DNAC_JOB_IDS}'.LoadConfig::getParam($rH_cfg, 'default', 'clusterDependencySep').$dnac1000JobId."\n";
+  my $dnac30000JobId = SubmitToCluster::printSubmitCmd($rH_cfg, "DNAC", undef, 'DNAC_30000', $jobDependency, $sampleName, $command30000);
+  print 'DNAC_JOB_IDS=${DNAC_JOB_IDS}'.LoadConfig::getParam($rH_cfg, 'default', 'clusterDependencySep').$dnac30000JobId."\n";
+
+  return $dnac30000JobId;
+}
 1;
