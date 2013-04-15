59e116ebfa09f2bec126de305b1471b2bd7b2cb7
Francois Lefebvre <lefebvrf@gmail.com>
2013-04-15 13:07:15 -0400

    Added hg19 installation

--- modules/genomes/hg19.sh
+++ modules/genomes/hg19.sh
@@ -1,5 +1,4 @@
 # TODO:
-# Improve rRNA definition
 # Bisulfite stuff...
 # Small RNA
 # Variation
@@ -13,25 +12,29 @@ module load mugqic/samtools
 module load mugqic/picard
 
 
+
+
 ##
-######### mm10
+######### hg19
 
 ## Fetch from iGenomes
-GENOME="mm10"
+GENOME="hg19"
 cd $MUGQIC_INSTALL_HOME/genomes
-wget "ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Mus_musculus/UCSC/mm10/Mus_musculus_UCSC_mm10.tar.gz"
-tar -xvf Mus_musculus_UCSC_mm10.tar.gz
-ROOT="$MUGQIC_INSTALL_HOME/genomes/Mus_musculus/UCSC/mm10"
+wget "ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Homo_sapiens/UCSC/hg19/Homo_sapiens_UCSC_hg19.tar.gz"
+tar -xvf Homo_sapiens_UCSC_hg19.tar.gz
+ROOT="$MUGQIC_INSTALL_HOME/genomes/Homo_sapiens/UCSC/hg19"
 
-# Bowtie1, Bowtie2, BWA indices
-# (already done ...  iGenomes)
+# bowtie-build Index reference (already done for iGenomes)
+#    bowtie-build /lb/project/mugqic/epigenome/genome_files/hg19/fasta/hg19.fasta /lb/project/mugqic/epigenome/genome_files/hg19/fasta/hg19.fasta # example Maxime
+# bwa index  reference (already done for iGenomes)
+#   bwa index -a bwtsw fasta/hg19.fasta # Example Maxime
 
-# fasta index samtools faidx
-# (already done ...  iGenomes)
+# faidx reference (already done for iGenomes)
+#   samtools faidx fasta/hg19.fasta # example Maxime
 
-# Reference dictionary (not done for iGenomes mm10)
-cd $ROOT/Sequence/WholeGenomeFasta
-java -jar $PICARD_HOME/CreateSequenceDictionary.jar REFERENCE=genome.fa  OUTPUT=genome.dict
+# reference dictionary (already done for iGenomes)
+#cd $ROOT/Sequence/WholeGenomeFasta
+#java -jar $PICARD_HOME/CreateSequenceDictionary.jar REFERENCE=genome.fa  
 
 
 ## Link WholeGenomeFasta/* in aligner indices  dirs
@@ -42,6 +45,9 @@ do
  ln -s "../WholeGenomeFasta/genome.dict"
 done
 
+
+
+
 ## Chrom size file, simple tab-delimied 1st col chr , 2nd col chr length. (already in iGenomes Annotation/Genes/ChromInfo.txt)
 # OR cut -f1,2 $ROOT/Sequence/WholeGenomeFasta/genome.fa.fai > $ROOT/Sequence/WholeGenomeFasta/genome.chromsize.txt
 # OR  sed 1d fasta/hg19.fasta.dict | awk '{print $2"\t"$3}' | sed -e 's/SN://g' | sed -e 's/LN://g' > hg19.chromsize.txt # example Maxime
@@ -49,12 +55,19 @@ done
 ## rRNA sequence + BWA index them (either from the Broad OR from iGenomes) (ln -s to). Put into $ROOT/Sequence/AbundantSequences/rRNA.fa
 # cat $ROOT/Sequence/AbundantSequences/hum5SrDNA.fa $ROOT/Sequence/AbundantSequences/humRibosomal.fa > $ROOT/Sequence/AbundantSequences/rRNA.fa
 # OR :
-cd $ROOT/Sequence/AbundantSequences
-ln -s musRibosomal.fa rRNA.fa
-bwa index rRNA.fa
+wget http://www.broadinstitute.org/cancer/cga/tools/rnaseqc/examples/rRNA/rRNA.tar.gz
+tar -zxvf rRNA.tar.gz 
+rm human_all_rRNA.fasta.*
+mv human_all_rRNA.fasta $ROOT/Sequence/AbundantSequences/rRNA.fa
+bwa index $ROOT/Sequence/AbundantSequences/rRNA.fa
 
 ## Gene models aka gtf.   (Already done for iGenomes, Annotation/Genes/genes.gtf)
-# ...
+# OR:
+# too complicated... ha
+
+## BioC TxDB
+# not working... use the txdb from Bioc???
+
 
 ## Gene size file for the gene model + gene id to symbol mapping. Easier to use GenomicRanges 
 cd $ROOT/Annotation/Genes
@@ -63,12 +76,12 @@ R --vanilla <<'EOF'
 	require(rtracklayer)
 	options(stringsAsFactors=FALSE)
 	x = import.gff('genes.gtf')
-	x$geneid = gsub('((gene_id |;)|\")','', str_extract(as.character(x$group),'gene_id.*?;') ) # MIGHT DIFFER FOR NON-UCSC GENOMES
+	x$geneid = gsub('((gene_id |;)|\")','', str_extract(as.character(x$group),'gene_id.*?;') )
 	g2s  = data.frame('ensembl'=unique(x$geneid),'geneSymbol'=unique(x$geneid))
 	write.table(g2s,file='genes_geneid2Symbol.txt',col.names=TRUE,row.names=FALSE,sep='\t',quote=FALSE)
 	x = x[x$type=='exon',]
 	x =  as(x,'GRanges') 		
-	x = split(x,f=factor(x$geneid))	 # split for GRanges signature turns into GRangesList!
+	x = split(x,f=factor(x$geneid))	
 	x = reduce(x) # reduce eliminates overlaps
 	x = sum ( width(x) )
 	x = as.data.frame(x)
@@ -78,14 +91,6 @@ cd -
 # OR Alternative is MAthieu's:
 # module load mugqic/tools
 # gtf2geneSize.awk <...>
-
-
-
-
-
-
-
-
 
 
 
