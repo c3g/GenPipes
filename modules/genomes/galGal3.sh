351be7c29f3447d14b4c25a6f3b3e2afc9e6c16b
lletourn <louis.letourneau@mail.mcgill.ca>
2013-05-08 15:00:29 -0400

    Cleaned up downloaded genomes

--- modules/genomes/galGal3.sh
+++ modules/genomes/galGal3.sh
@@ -13,9 +13,10 @@ mkdir -p $MUGQIC_INSTALL_HOME/genomes/${SPECIE}/${GENOME}/annotations
 cd $MUGQIC_INSTALL_HOME/genomes/${SPECIE}/${GENOME}/fasta/byChr
 wget "http://hgdownload.soe.ucsc.edu/goldenPath/galGal3/bigZips/chromFa.tar.gz"
 tar xzvf chromFa.tar.gz
+rm chromFa.tar.gz
 cat chr[0-9].fa chr[1-9][0-9].fa chr[WZM].fa chrE64.fa chr*_*.fa > ../${GENOME}.fasta
 
 # Reference dictionary (not done for iGenomes mm10)
-cd ../
+cd $MUGQIC_INSTALL_HOME/genomes/${SPECIE}/${GENOME}/fasta
 java -Xmx1G -jar $PICARD_HOME/CreateSequenceDictionary.jar REFERENCE=${GENOME}.fasta  OUTPUT=${GENOME}.fasta.dict GENOME_ASSEMBLY=${GENOME} SPECIES=${SPECIE}
 
