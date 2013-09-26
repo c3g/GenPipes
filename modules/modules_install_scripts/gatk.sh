89ac0f93959ffed0360c1b75b3229409826cda28
Joël Fillon <joel.fillon@mcgill.ca>
2013-09-26 12:18:34 -0400

    Added permissions + minor fix in gatk module install script

--- modules/modules_install_scripts/gatk.sh
+++ modules/modules_install_scripts/gatk.sh
@@ -7,9 +7,12 @@ echo "You must download and install GATK manually";
 echo "It can be found here:"
 echo "http://www.broadinstitute.org/gatk/download"
 VERSION="2.5-2-gf57256b"
-INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/GenomeAnalysisTK/GenomeAnalysisTK-${VERSION}
+# Remove the version trailing characters
+SHORT_VERSION=`echo $VERSION | sed 's/-[^-]*$//'`
+INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/GenomeAnalysisTK/GenomeAnalysisTK-$VERSION
 mkdir -p $INSTALL_PATH
-echo "Install it here: ${INSTALL_PATH}"
+echo "Install it here: $INSTALL_PATH"
+chmod -R g+w $INSTALL_PATH
 
 echo "#%Module1.0
 
@@ -19,16 +22,16 @@ proc ModulesHelp { } {
 
 module-whatis "The Broads toolsuite to work with resequencing"
 
-set             root         $::env(MUGQIC_INSTALL_HOME)/software/GenomeAnalysisTK/GenomeAnalysisTK-${VERSION}
-setenv          GATK_JAR     $root/GenomeAnalysisTK.jar
-" > $VERSION
+set             root         $::env(MUGQIC_INSTALL_HOME)/software/GenomeAnalysisTK/GenomeAnalysisTK-$VERSION
+setenv          GATK_JAR     \$root/GenomeAnalysisTK.jar
+" > $SHORT_VERSION
 
-# version file
+# Version file
 echo "#%Module1.0
-set ModulesVersion \"$VERSION\"
+set ModulesVersion \"$SHORT_VERSION\"
 " > .version
 
 mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/GenomeAnalysisTK
-mv .version $VERSION $MUGQIC_INSTALL_HOME/modulefiles/mugqic/GenomeAnalysisTK/
+mv .version $SHORT_VERSION $MUGQIC_INSTALL_HOME/modulefiles/mugqic/GenomeAnalysisTK/
 
 echo "Module is installed here: $MUGQIC_INSTALL_HOME/modulefiles/mugqic/GenomeAnalysisTK"
