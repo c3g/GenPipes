7f60581a2e08ef82280841aeca7f499f7be57571
mathieu bourgey <mathieu.bourgey@mail.mcgill.ca>
2013-11-12 12:48:51 -0500

    update breakdancer module

--- modules/breakdancer.sh
+++ modules/breakdancer.sh
@@ -3,7 +3,7 @@
 ###################
 ################### Samtools
 ###################
-VERSION="1.1.2_2013_03_08"
+VERSION="1.1_2011_02_21"
 
 INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/breakdancer/
 mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/breakdancer/tmp/unzip
@@ -19,18 +19,8 @@ cd unzip/breakdancer-${VERSION}/
 wget http://downloads.sourceforge.net/project/samtools/samtools/0.1.6/samtools-0.1.6.tar.bz2
 tar xvjf samtools-0.1.6.tar.bz2
 
-# Compile samtools
-cd samtools-0.1.6
-make -j8
 
-#install
-cd ../breakdancer-${VERSION}/cpp
-mv Makefile originalMakefile
-sed "s|/Users/kchen3/pkg/samtools/samtools-0\.1\.6|\.\./samtools-0\.1\.6|g" originalMakefile > Makefile
-
-make
-
-cd ../../..
+cd ../..
 
 INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/breakdancer # where to install..
 ARCHIVE_PATH=$MUGQIC_INSTALL_HOME/archive/breakdancer 
@@ -39,6 +29,25 @@ cp -r unzip/breakdancer-${VERSION}  $INSTALL_PATH
 chmod -R 775 $INSTALL_PATH 
 mv breakdancer-${VERSION}.zip $ARCHIVE_PATH
 mv unzip/breakdancer-${VERSION}/samtools-0.1.6.tar.bz2 $ARCHIVE_PATH
+
+cd ${INSTALL_PATH}/breakdancer-${VERSION}/samtools-0.1.6
+make -j8
+
+
+#install
+cd ../cpp
+mv Makefile originalMakefile
+sed "s|/Users/kchen3/pkg/samtools/samtools-0\.1\.6|${INSTALL_PATH}/breakdancer-${VERSION}/samtools-0\.1\.6|g" originalMakefile > Makefile
+
+make
+
+
+cd ../perl
+mv bam2cfg.pl originalBam2cfg.pl 
+sed "s|/opt/local/bin/perl|/usr/bin/perl|g" originalBam2cfg.pl > bam2cfg.pl
+chmod 775 bam2cfg.pl
+
+cd $MUGQIC_INSTALL_HOME/modulefiles/mugqic/breakdancer/tmp
 
 
 # Module file
@@ -51,6 +60,7 @@ module-whatis \"Breakdancer Structural Variant analyser\"
 set             root               \$::env(MUGQIC_INSTALL_HOME)/software/breakdancer/breakdancer-${VERSION}
 prepend-path    PATH               \$root/cpp/
 prepend-path    PATH               \$root/perl/
+prepend-path    PATH               \$root/samtools-0.1.6/
 setenv          BRD_CPP            \$root/cpp/
 setenv          BRD_PERL            \$root/perl/
 
