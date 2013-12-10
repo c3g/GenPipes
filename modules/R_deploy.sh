99a908445cdbed2d40627a38da81efc5629615d4
Francois Lefebvre <lefebvrf@gmail.com>
2013-12-10 17:08:44 -0500

    wrong file name in cron script

--- modules/R_deploy.sh
+++ modules/R_deploy.sh
@@ -17,8 +17,28 @@ ssh lefebvr3@bourque-mp2.rqchp.ca  "bash -l -s" -- >& mammouth.R.log  < R.sh $@
 exit
 
 # wget https://bitbucket.org/mugqic/mugqic_resources/raw/master/modules/R_deploy.sh -O R_deploy.sh && wget https://bitbucket.org/mugqic/mugqic_resources/raw/master/modules/R.sh -O R.sh
+
 # sh R_deploy.sh -f -v 3.0.0
 # sh R_deploy.sh -f -v 3.0.2
+
+
+
+# sh R.sh -f -v 3.0.0
+# sh R.sh -f -v 3.0.2
 # sh R.sh -f -m $MUGQIC_INSTALL_HOME/modulefiles/mugqic/R -i $MUGQIC_INSTALL_HOME/software/R -v 3.0.0
 # sh R.sh -f -m $MUGQIC_INSTALL_HOME/modulefiles/mugqic/R -i $MUGQIC_INSTALL_HOME/software/R -v 3.0.2
+
+# ls -alrth $MUGQIC_INSTALL_HOME_DEV/software/R/R-3.0.0/lib64/R/library/
+# ls -alrth $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic_dev/R/3.0.0
+# 
+# ls -alrth $MUGQIC_INSTALL_HOME_DEV/software/R/R-3.0.2/lib64/R/library/
+# ls -alrth $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic_dev/R/3.0.2
+# 
+# ls -alrth  $MUGQIC_INSTALL_HOME/software/R/R-3.0.0/lib64/R/library/
+# ls -alrth  $MUGQIC_INSTALL_HOME/modulefiles/mugqic/R/3.0.0
+# 
+# ls -alrth  $MUGQIC_INSTALL_HOME/software/R/R-3.0.2/lib64/R/library/
+# ls -alrth  $MUGQIC_INSTALL_HOME/modulefiles/mugqic/R/3.0.2
+
+
 
