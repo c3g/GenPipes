9b4f23fe4005fe37865546759309afdeb19b3aa3
Joël Fillon <joel.fillon@mcgill.ca>
2013-12-11 16:06:44 -0500

    Standardized Trimmomatic module install

--- modules/trimmomatic.sh
+++ modules/trimmomatic.sh
@@ -1,41 +1,50 @@
+#!/bin/sh
 
+#
+# Trimmomatic
+#
 
-###################
-################### Trimmomatic
-###################
-VERSION=0.30
-INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/trimmomatic
+SOFTWARE=trimmomatic
+VERSION=0.32
+INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/$SOFTWARE
 INSTALL_DOWNLOAD=$INSTALL_PATH/tmp
-mkdir -p $INSTALL_PATH/archive $INSTALL_DOWNLOAD
+mkdir -p $INSTALL_DOWNLOAD
 cd $INSTALL_DOWNLOAD
 
-# Download and extract
+# Download, extract, build
 wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-$VERSION.zip
 unzip Trimmomatic-$VERSION.zip
 
-# Install
-mv Trimmomatic-$VERSION $INSTALL_PATH
-cd $INSTALL_PATH
-chmod -R g+w Trimmomatic-$VERSION
+# Add permissions and install software
+cd $INSTALL_DOWNLOAD
+chmod -R ug+rwX .
+chmod -R o+rX .
+mv -i Trimmomatic-$VERSION $INSTALL_PATH
+mv -i Trimmomatic-$VERSION.zip $MUGQIC_INSTALL_HOME/archive
 
 # Module file
 echo "#%Module1.0
 proc ModulesHelp { } {
-       puts stderr \"\tMUGQIC - Trimmomatic to trim fastq \"
+       puts stderr \"\tMUGQIC - Trimmomatic to trim fastq \" ;
 }
-module-whatis \"Trimmomatic to trim fastq  \"
+module-whatis \"Trimmomatic to trim fastq  \" ;
                       
-set             root                \$::env(MUGQIC_INSTALL_HOME)/software/trimmomatic/Trimmomatic-$VERSION
-setenv          TRIMMOMATIC_JAR     \$root/trimmomatic-$VERSION.jar
-
+set             root                \$::env(MUGQIC_INSTALL_HOME)/software/$SOFTWARE/Trimmomatic-$VERSION ;
+setenv          TRIMMOMATIC_JAR     \$root/$SOFTWARE-$VERSION.jar ;
 " > $VERSION
 
-# Version file
-echo "#%Module1.0
-set ModulesVersion \"$VERSION\"
-" > .version
+################################################################################
+# Everything below this line should be generic and not modified
 
-mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/trimmomatic
-mv .version $VERSION $MUGQIC_INSTALL_HOME/modulefiles/mugqic/trimmomatic/
-mv $INSTALL_DOWNLOAD/Trimmomatic-$VERSION.zip $INSTALL_PATH/archive/
+# Default module version file
+echo "#%Module1.0
+set ModulesVersion \"$VERSION\"" > .version
+
+# Add permissions and install module
+mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
+chmod -R ug+rwX $VERSION .version
+chmod -R o+rX $VERSION .version
+mv $VERSION .version $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
+
+# Clean up temporary installation files if any
 rm -rf $INSTALL_DOWNLOAD
