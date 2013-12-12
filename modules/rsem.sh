ea2d0aeb85d335e08e27bae44aba58ca59c5d5de
Joël Fillon <joel.fillon@mcgill.ca>
2013-12-12 11:08:00 -0500

    Installed trinity and rsem in production

--- modules/rsem.sh
+++ modules/rsem.sh
@@ -6,7 +6,7 @@
 
 SOFTWARE=rsem
 VERSION=1.2.8
-INSTALL_PATH=$MUGQIC_INSTALL_HOME_DEV/software/$SOFTWARE
+INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/$SOFTWARE
 INSTALL_DOWNLOAD=$INSTALL_PATH/tmp
 mkdir -p $INSTALL_DOWNLOAD
 cd $INSTALL_DOWNLOAD
@@ -22,7 +22,7 @@ cd $INSTALL_DOWNLOAD
 chmod -R ug+rwX .
 chmod -R o+rX .
 mv -i $SOFTWARE-$VERSION $INSTALL_PATH
-mv -i $SOFTWARE-$VERSION.tar.gz $MUGQIC_INSTALL_HOME_DEV/archive
+mv -i $SOFTWARE-$VERSION.tar.gz $MUGQIC_INSTALL_HOME/archive
 
 # Module file
 echo "#%Module1.0
@@ -31,7 +31,7 @@ proc ModulesHelp { } {
 }
 module-whatis \"$SOFTWARE  \" ;
                       
-set             root                \$::env(MUGQIC_INSTALL_HOME_DEV)/software/$SOFTWARE/$SOFTWARE-$VERSION ;
+set             root                \$::env(MUGQIC_INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE-$VERSION ;
 prepend-path    PATH                \$root ;
 " > $VERSION
 
@@ -43,10 +43,10 @@ echo "#%Module1.0
 set ModulesVersion \"$VERSION\"" > .version
 
 # Add permissions and install module
-mkdir -p $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic_dev/$SOFTWARE
+mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
 chmod -R ug+rwX $VERSION .version
 chmod -R o+rX $VERSION .version
-mv $VERSION .version $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic_dev/$SOFTWARE
+mv $VERSION .version $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
 
 # Clean up temporary installation files if any
 rm -rf $INSTALL_DOWNLOAD
