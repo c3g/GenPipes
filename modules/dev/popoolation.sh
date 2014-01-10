a8a9a0f7c6b72cbbc3cf7a8a6effd70c538fccd6
johanna.sandoval@mail.mcgill.ca <johanna.sandoval@mail.mcgill.ca>
2014-01-10 12:15:47 -0500

    added env-variable POPOOLATION_HOME to the module file

--- modules/dev/popoolation.sh
+++ modules/dev/popoolation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-SOFTWARE="popoolation"
-VERSION="0.218"  ## Checked out svn revision 218
+SOFTWARE="popoolation2"
+VERSION="0.189"  ## Checked out svn revision 189
 INSTALL_PATH=$MUGQIC_INSTALL_HOME_DEV/software/$SOFTWARE
 INSTALL_DOWNLOAD=$INSTALL_PATH/tmp
 mkdir -p $INSTALL_DOWNLOAD
@@ -9,15 +9,16 @@ cd $INSTALL_DOWNLOAD
 
 # Download, extract, build
 # Write here the specific commands to download, extract, build the software, typically similar to:
-svn checkout http://popoolation.googlecode.com/svn $SOFTWARE
+svn checkout http://popoolation2.googlecode.com/svn/trunk/ $SOFTWARE-$VERSION
+svn update
 
 # Add permissions and install software
 cd $INSTALL_DOWNLOAD
 chmod -R ug+rwX .
 chmod -R o+rX .
-mv -i $SOFTWARE-$VERSION $INSTALL_PATH  ## TO BE MODIFIED WITH SPECIFIC $SOFTWARE-$VERSION IF DIFFERENT
-zip -r $SOFTWARE-$VERSION.zip popoolation/*
-mv -i $SOFTWARE-$VERSION.zip $MUGQIC_INSTALL_HOME_DEV/archive  ## TO BE MODIFIED WITH SPECIFIC $SOFTWARE-$VERSION.(zip|tar.gz|tar.bz2) IF DIFFERENT
+zip -r $SOFTWARE-$VERSION.zip $SOFTWARE-$VERSION/*
+mv -i $SOFTWARE-$VERSION $INSTALL_PATH
+mv -i $SOFTWARE-$VERSION.zip $MUGQIC_INSTALL_HOME_DEV/archive  
 
 # Module file
 echo "#%Module1.0
@@ -26,10 +27,10 @@ proc ModulesHelp { } {
 }
 module-whatis \"$SOFTWARE  is designed to analyze next generation sequencing data of pooled genomic DNA.\" ;  
                       
-set             root                \$::env(MUGQIC_INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE-$VERSION ;  
-prepend-path    PATH                \$root/popoolation/ ;  
-prepend-path    PATH                \$root/other_tools/bin ; 
-setenv          PERL5LIB            \$root/popoolation/Modules ; 
+set             root                \$::env(MUGQIC_INSTALL_HOME_DEV)/software/$SOFTWARE/$SOFTWARE-$VERSION ;  
+prepend-path    PATH                \$root;  
+setenv          PERL5LIB            \$root/Modules; 
+setenv          POPOOLATION_PATH    \$root;
 " > $VERSION
 
 ################################################################################
@@ -40,10 +41,10 @@ echo "#%Module1.0
 set ModulesVersion \"$VERSION\"" > .version
 
 # Add permissions and install module
-mkdir -p $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic/$SOFTWARE
+mkdir -p $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic_dev/$SOFTWARE
 chmod -R ug+rwX $VERSION .version
 chmod -R o+rX $VERSION .version
-mv $VERSION .version $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic/$SOFTWARE
+mv $VERSION .version $MUGQIC_INSTALL_HOME_DEV/modulefiles/mugqic_dev/$SOFTWARE
 
 # Clean up temporary installation files if any
 rm -rf $INSTALL_DOWNLOAD
