201cc7d70038e4cd06b32891c5abb20406a6569f
Joël Fillon <joel.fillon@mcgill.ca>
2014-02-27 16:37:31 -0500

    Standardized bowtie, exonerate, samtools module install scripts

--- modules/bowtie2.sh
+++ modules/bowtie2.sh
@@ -1,41 +1,52 @@
-###################
-################### Bowtie2
-###################
-VERSION="2.2.0"
-INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/bowtie/bowtie-$VERSION
-INSTALL_DOWNLOAD=$MUGQIC_INSTALL_HOME/software/bowtie/tmp
-mkdir -p $INSTALL_PATH $INSTALL_DOWNLOAD
+#!/bin/sh
 
-# Download and extract
+#
+# Bowtie2
+#
+
+SOFTWARE=bowtie2
+VERSION=2.2.0
+INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/$SOFTWARE
+INSTALL_DOWNLOAD=$INSTALL_PATH/tmp
+mkdir -p $INSTALL_DOWNLOAD
 cd $INSTALL_DOWNLOAD
-wget http://sourceforge.net/projects/bowtie-bio/files/bowtie2/$VERSION/bowtie2-$VERSION-source.zip/download
-unzip bowtie2-$VERSION-source.zip
 
-# Compile
-cd bowtie2-$VERSION
-make -j8
-mv bowtie2* $INSTALL_PATH
-chmod -R g+w $INSTALL_PATH
-cd ..
+# Download, extract, build
+wget http://sourceforge.net/projects/bowtie-bio/files/$SOFTWARE/$VERSION/$SOFTWARE-$VERSION-source.zip
+unzip $SOFTWARE-$VERSION-source.zip
+cd $SOFTWARE-$VERSION
+make
 
+# Add permissions and install software
+cd $INSTALL_DOWNLOAD
+chmod -R ug+rwX .
+chmod -R o+rX .
+mv -i $SOFTWARE-$VERSION $INSTALL_PATH
+mv -i $SOFTWARE-$VERSION-source.zip $MUGQIC_INSTALL_HOME/archive
 
 # Module file
 echo "#%Module1.0
 proc ModulesHelp { } {
-       puts stderr \"\tMUGQIC - Bowtie2 aligner \"
+       puts stderr \"\tMUGQIC - $SOFTWARE \" ;
 }
-module-whatis \"MUGQIC - Bowtie2 aligner \"
+module-whatis \"$SOFTWARE  \" ;
                       
-set             root               \$::env(MUGQIC_INSTALL_HOME)/software/bowtie/bowtie-$VERSION
-prepend-path    PATH               \$root
+set             root                \$::env(MUGQIC_INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE-$VERSION ;
+prepend-path    PATH                \$root
 " > $VERSION
 
-# Version file
+################################################################################
+# Everything below this line should be generic and not modified
+
+# Default module version file
 echo "#%Module1.0
-set ModulesVersion \"$VERSION\"
-" > .version
+set ModulesVersion \"$VERSION\"" > .version
 
-mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/bowtie
-mv .version $VERSION $MUGQIC_INSTALL_HOME/modulefiles/mugqic/bowtie/
+# Add permissions and install module
+mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
+chmod -R ug+rwX $VERSION .version
+chmod -R o+rX $VERSION .version
+mv $VERSION .version $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
 
+# Clean up temporary installation files if any
 rm -rf $INSTALL_DOWNLOAD
--- modules/exonerate.sh
+++ modules/exonerate.sh
@@ -1,43 +1,51 @@
+#!/bin/sh
 
+#
+# Exonerate
+#
 
-###################
-################### exonerate
-###################
-VERSION="2.2.0"
-INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/exonerate/
-mkdir -p $INSTALL_PATH
-cd $INSTALL_PATH
+SOFTWARE=exonerate
+VERSION=2.2.0
+INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/$SOFTWARE
+INSTALL_DOWNLOAD=$INSTALL_PATH/tmp
+mkdir -p $INSTALL_DOWNLOAD
+cd $INSTALL_DOWNLOAD
 
-# Download
-wget http://www.ebi.ac.uk/~guy/exonerate/exonerate-${VERSION}.tar.gz
-tar xvzf exonerate-${VERSION}.tar.gz
+# Download, extract, build
+wget http://www.ebi.ac.uk/~guy/$SOFTWARE/$SOFTWARE-$VERSION-x86_64.tar.gz
+tar zxvf $SOFTWARE-$VERSION-x86_64.tar.gz
 
-# Compile
-mv exonerate-${VERSION} exonerate-${VERSION}-src
-cd exonerate-${VERSION}-src
-./configure --prefix ${INSTALL_PATH}/exonerate-${VERSION}
-make -j8
-make install && rm -r ../exonerate-${VERSION}-src
-cd $INSTALL_PATH
-chmod -R g+w exonerate-${VERSION}
+# Add permissions and install software
+cd $INSTALL_DOWNLOAD
+chmod -R ug+rwX .
+chmod -R o+rX .
+mv -i $SOFTWARE-$VERSION-x86_64 $INSTALL_PATH
+mv -i $SOFTWARE-$VERSION-x86_64.tar.gz $MUGQIC_INSTALL_HOME/archive
 
 # Module file
 echo "#%Module1.0
 proc ModulesHelp { } {
-       puts stderr \"\tMUGQIC - exonerate \"
+       puts stderr \"\tMUGQIC - $SOFTWARE \"
 }
-module-whatis \"Exonerate is a generic tool for pairwise sequence comparison and fasta manipulation\"
-            
-set             root               \$::env(MUGQIC_INSTALL_HOME)/software/exonerate/exonerate-${VERSION}
-prepend-path    PATH               \$root/bin
-prepend-path    MANPATH            \$root/share/man/
+module-whatis \"$SOFTWARE is a generic tool for pairwise sequence comparison and fasta manipulation \"
+
+set             root                \$::env(MUGQIC_INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE-$VERSION-x86_64
+prepend-path    PATH                \$root/bin
+prepend-path    MANPATH             \$root/share/man/
 " > $VERSION
 
-# Version file
-echo "#%Module1.0
-set ModulesVersion \"$VERSION\"
-" > .version
+################################################################################
+# Everything below this line should be generic and not modified
 
-mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/exonerate
-mv .version $VERSION $MUGQIC_INSTALL_HOME/modulefiles/mugqic/exonerate/
-rm $INSTALL_PATH/exonerate-${VERSION}.tar.gz
+# Default module version file
+echo "#%Module1.0
+set ModulesVersion \"$VERSION\"" > .version
+
+# Add permissions and install module
+mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
+chmod -R ug+rwX $VERSION .version
+chmod -R o+rX $VERSION .version
+mv $VERSION .version $MUGQIC_INSTALL_HOME/modulefiles/mugqic/$SOFTWARE
+
+# Clean up temporary installation files if any
+rm -rf $INSTALL_DOWNLOAD
