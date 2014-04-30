610973909a190b702559f8af2f8838e2347f71e7
lletourn <louis.letourneau@mail.mcgill.ca>
2014-04-30 08:52:37 -0400

    Added the jellyfish tool

---
+++ modules/jellyfish.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#
+# Jellyfish
+#
+
+SOFTWARE=jellyfish
+VERSION=2.1.3
+
+INSTALL_HOME=MUGQIC_INSTALL_HOME
+
+# Indirection call to use $INSTALL_HOME value as variable name
+INSTALL_DIR=${!INSTALL_HOME}/software/$SOFTWARE
+
+# Create install directory with permissions if necessary
+if [[ ! -d $INSTALL_DIR ]]
+then
+  mkdir $INSTALL_DIR
+  chmod ug+rwX,o+rX $INSTALL_DIR
+fi
+
+INSTALL_DOWNLOAD=$INSTALL_DIR/tmp
+mkdir $INSTALL_DOWNLOAD
+cd $INSTALL_DOWNLOAD
+
+# Download, extract, build
+# Write here the specific commands to download, extract, build the software, typically similar to:
+ARCHIVE=$SOFTWARE-$VERSION.tar.gz
+wget ftp://ftp.genome.umd.edu/pub/jellyfish/$ARCHIVE
+tar zxvf $ARCHIVE
+
+SOFTWARE_DIR=$SOFTWARE-$VERSION
+cd $SOFTWARE_DIR
+./configure --prefix=$INSTALL_DIR/$SOFTWARE_DIR
+make
+make install
+
+# Add permissions and install software
+cd $INSTALL_DOWNLOAD
+chmod -R ug+rwX,o+rX .
+mv -i $ARCHIVE ${!INSTALL_HOME}/archive
+
+# Module file
+echo "#%Module1.0
+proc ModulesHelp { } {
+  puts stderr \"\tMUGQIC - $SOFTWARE \" ;
+}
+module-whatis \"$SOFTWARE\" ;
+
+set             root                \$::env($INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE_DIR
+prepend-path    PATH                \$root/bin ;  ## TO BE ADDED IF NECESSARY
+" > $VERSION
+
+################################################################################
+# Everything below this line should be generic and not modified
+
+# Default module version file
+echo "#%Module1.0
+set ModulesVersion \"$VERSION\"" > .version
+
+# Set module directory path by removing '_INSTALL_HOME' in $INSTALL_HOME and lowercasing the result
+MODULE_DIR=${!INSTALL_HOME}/modulefiles/`echo ${INSTALL_HOME/_INSTALL_HOME/} | tr '[:upper:]' '[:lower:]'`/$SOFTWARE
+
+# Create module directory with permissions if necessary
+if [[ ! -d $MODULE_DIR ]]
+then
+  mkdir $MODULE_DIR
+  chmod ug+rwX,o+rX $MODULE_DIR
+fi
+
+# Add permissions and install module
+chmod ug+rwX,o+rX $VERSION .version
+mv $VERSION .version $MODULE_DIR
+
+# Clean up temporary installation files if any
+rm -rf $INSTALL_DOWNLOAD
