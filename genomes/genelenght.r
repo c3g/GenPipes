923d66198ad17a5e9cf8cd261db36546b067c1e1
lefebvrf <francois.lefebvre3@mail.mcgill.ca>
2014-06-10 16:47:30 -0400

    Removed genelenght.r. Use mugqic_tools/R-tools or gqSeqUtils directly with HERE docs or Rscript -e

--- genomes/genelenght.r
+++
@@ -1,4 +0,0 @@
-library(gqSeqUtils)
-le = calculateGeneLengthsFromGtf("BosTau7.gtf")
-le = data.frame(le,row.names=names(le))
-write.table(le,file='gene_lengths.tsv',sep='\t',col.names=FALSE,row.names=TRUE,quote=FALSE)
