a1466e78f2044a9ec26d5ebbfa3990991b747af7
Joël Fillon <joel.fillon@mcgill.ca>
2014-08-22 17:48:15 -0400

    GENOME_INSTALL_TEMPLATE with Ensembl support

--- genomes/GENOME_INSTALL_TEMPLATE.sh
+++ genomes/GENOME_INSTALL_TEMPLATE.sh
@@ -11,72 +11,184 @@ set -eu -o pipefail
 
 
 SPECIES=Species_scientific_name  ## TO BE MODIFIED WITH e.g. Homo_sapiens, Canis_familiaris, Arabidopsis_thaliana, etc.
-SPECIES=Saccharomyces_cerevisiae
+#SPECIES=Saccharomyces_cerevisiae
+#SPECIES=Caenorhabditis_elegans
+#SPECIES=Mus_musculus
+SPECIES=Homo_sapiens
+#SPECIES=Escherichia_coli_str_k_12_substr_dh10b
+#SPECIES=Arabidopsis_thaliana
+#SPECIES=Schizosaccharomyces_pombe
 ASSEMBLY=Source_assembly_version0.0  ## TO BE MODIFIED WITH e.g. GRCh37, CanFam3.1, TAIR10, etc.
-ASSEMBLY=R64-1-1
+#ASSEMBLY=R64-1-1
+#ASSEMBLY=WBcel235
+#ASSEMBLY=GRCm38
+ASSEMBLY=GRCh38
+#ASSEMBLY=ASM1942v1
+#ASSEMBLY=TAIR10
+#ASSEMBLY=ASM294v2
+#SOURCE=(Ensembl|Ensembl_Genomes)  ## TO BE MODIFIED WITH SPECIFIC SOURCE
+#SOURCE=Ensembl_Genomes
+SOURCE=Ensembl
+RELEASE=76  ## TO BE MODIFIED WITH SPECIFIC Ensembl(_Genomes) RELEASE IF NECESSARY
+BASENAME=$SPECIES.$ASSEMBLY.$RELEASE
 
-# 'MUGQIC_INSTALL_HOME_DEV' for development, 'MUGQIC_INSTALL_HOME' for production (don't write '$' before!)
-INSTALL_HOME=MUGQIC_INSTALL_HOME_DEV  ## TO BE MODIFIED IF NECESSARY
+# '$MUGQIC_INSTALL_HOME_DEV' for development, '$MUGQIC_INSTALL_HOME' for production
+INSTALL_HOME=$MUGQIC_INSTALL_HOME_DEV  ## TO BE MODIFIED IF NECESSARY
 
-# Indirection call to use $INSTALL_HOME value as variable name
-INSTALL_DIR=${!INSTALL_HOME}/genomes/species/$SPECIES/$ASSEMBLY
+INSTALL_DIR=$INSTALL_HOME/genomes/species/$SPECIES.$ASSEMBLY
+SOURCE_DIR=$INSTALL_DIR/source/$SOURCE
+LOG_DIR=$INSTALL_DIR/log
+TIMESTAMP=`date +%FT%H.%M.%S`
+
+# Standard name for our genome FASTA file whatever the source
+GENOME_DIR=$INSTALL_DIR/genome
+GENOME_BASENAME=$SPECIES.$ASSEMBLY
+GENOME_FASTA=$GENOME_BASENAME.fa
+GTF=$BASENAME.gtf
+NCRNA=$BASENAME.ncrna.fa
+
+echo Installing genome for:
+echo species: $SPECIES
+echo assembly: $ASSEMBLY
+echo source: $SOURCE
+echo in: $INSTALL_DIR
+echo
 
 # Create install directory with permissions if necessary
-if [[ ! -d $INSTALL_DIR ]]
-then
-  mkdir -p $INSTALL_DIR
-  chmod ug+rwX,o+rX $INSTALL_DIR
-fi
+if [[ ! -d $INSTALL_DIR ]] ; then mkdir -p $INSTALL_DIR ; fi
 
-#SOURCE=(Ensembl|UCSC)  ## TO BE MODIFIED WITH SPECIFIC SOURCE
-SOURCE=Ensembl
+# Create subdirectories
+mkdir -p $SOURCE_DIR $LOG_DIR $GENOME_DIR $INSTALL_DIR/annotations
+
+cd $SOURCE_DIR
 
 #
-# Ensembl
+# Ensembl (vertebrate species)
 #
 if [[ $SOURCE == "Ensembl" ]]
 then
 
-ENSEMBL_RELEASE=75  ## TO BE MODIFIED IF NECESSARY
-ENSEMBL_URL_PREFIX=ftp://ftp.ensembl.org/pub/release-$ENSEMBL_RELEASE
+URL_PREFIX=ftp://ftp.ensembl.org/pub/release-$RELEASE
+#GENOME_URL=$URL_PREFIX/fasta/${SPECIES,,}/dna/$GENOME_BASENAME.dna.(primary_assembly|toplevel).fa.gz  ## TO BE MODIFIED WITH SPECIFIC GENOME URL
+GENOME_URL=$URL_PREFIX/fasta/${SPECIES,,}/dna/$GENOME_BASENAME.dna.primary_assembly.fa.gz
+NCRNA_URL=$URL_PREFIX/fasta/${SPECIES,,}/ncrna/$GENOME_BASENAME.ncrna.fa.gz
+GTF_URL=$URL_PREFIX/gtf/${SPECIES,,}/$BASENAME.gtf.gz
 
-BASENAME=$SPECIES.$ASSEMBLY.$ENSEMBL_RELEASE
-DNA_URL=$ENSEMBL_URL_PREFIX/fasta/${SPECIES,,}/dna/$BASENAME.dna.toplevel.fa.gz
-DNA_FASTA=$BASENAME.fa
-GTF_URL=$ENSEMBL_URL_PREFIX/gtf/${SPECIES,,}/$BASENAME.gtf.gz
-rRNA_URL=$ENSEMBL_URL_PREFIX/fasta/${SPECIES,,}/ncrna/$BASENAME.ncrna.fa.gz
+#
+# Ensembl Genomes (non-vertebrate species)
+#
+elif [[ $SOURCE == "Ensembl_Genomes" ]]
+then
 
+RELEASE_URL=ftp://ftp.ensemblgenomes.org/pub/release-$RELEASE
+SPECIES_URL=$RELEASE_URL/species.txt
+wget $SPECIES_URL
+# Retrieve species division (Bacteria|Fungi|Metazoa|Plants|Protists)
+DIVISION=`cut -f2,3 species.txt | grep -i ${SPECIES,,} | cut -f2 | sed "s/^Ensembl//"`
+
+# Escherichia coli bacteria file paths are different
+# Retrieve which bacteria collection it belongs to
+CORE_DB_PREFIX=`cut -f2,13 species.txt | grep -i ${SPECIES,,} | cut -f2 | perl -pe "s/_core_${RELEASE}_\d+_\d+//"`
+if [[ $CORE_DB_PREFIX != ${SPECIES,,} ]]
+then
+EG_SPECIES=$CORE_DB_PREFIX/${SPECIES,,}
+EG_BASENAME=$SPECIES.`cut -f2,6 species.txt | grep -i ${SPECIES,,} | cut -f2`.$RELEASE
+else
+EG_SPECIES=${SPECIES,,}
+EG_BASENAME=$BASENAME
 fi
 
-cd $INSTALL_DIR
+URL_PREFIX=$RELEASE_URL/${DIVISION,}
+GENOME_URL=$URL_PREFIX/fasta/$EG_SPECIES/dna/$EG_BASENAME.dna.genome.fa.gz
+NCRNA_URL=$URL_PREFIX/fasta/$EG_SPECIES/ncrna/$EG_BASENAME.ncrna.fa.gz
+GTF_URL=$URL_PREFIX/gtf/$EG_SPECIES/$EG_BASENAME.gtf.gz
+fi
 
-echo Installing genome assembly...
-mkdir -p $INSTALL_DIR/source
-cd $INSTALL_DIR/source
-wget $DNA_URL
-mkdir -p $INSTALL_DIR/dna
-gunzip -c `basename $DNA_URL` > $INSTALL_DIR/dna/$DNA_FASTA
-cd $INSTALL_DIR/dna
-#ln -s -f `basename ${DNA_URL%.gz}` $SPECIES.$ASSEMBLY.fa
+echo
+echo Downloading $GENOME_URL...
+echo
+#wget $GENOME_URL
+#gunzip -c `basename $GENOME_URL` > $GENOME_DIR/$GENOME_FASTA
 
-echo Creating genome SAM dictionary...
-module load mugqic/picard mugqic/java
-java -jar $PICARD_HOME/CreateSequenceDictionary.jar REFERENCE=$DNA_FASTA OUTPUT=$BASENAME.dict GENOME_ASSEMBLY=$BASENAME
+echo
+echo Downloading $GTF_URL...
+echo
+#wget $GTF_URL
+#gunzip -c `basename $GTF_URL` > $INSTALL_DIR/annotations/$GTF
 
-echo Creating genome FASTA index...
-module load mugqic/samtools
-samtools faidx $DNA_FASTA
+echo
+echo Downloading $NCRNA_URL...
+echo
+wget $NCRNA_URL
+gunzip -c `basename $NCRNA_URL` > $INSTALL_DIR/annotations/$NCRNA
 
-echo Creating genome bwa index...
-mkdir -p $INSTALL_DIR/dna/bwa
-cd $INSTALL_DIR/dna/bwa
-ln -s -f ../$DNA_FASTA
-module load mugqic/bwa
-bwa index $DNA_FASTA
+# Create indexes directories
+for INDEX_DIR in bowtie2_index bwa_index picard_index sam_index
+do
+  mkdir -p $GENOME_DIR/$INDEX_DIR
+  cd $GENOME_DIR/$INDEX_DIR
+  ln -s -f -t $GENOME_DIR/$INDEX_DIR ../$GENOME_FASTA
+done
 
-echo Creating genome bowtie2 index...
-mkdir -p $INSTALL_DIR/dna/bowtie2
-cd $INSTALL_DIR/dna/bowtie2
-ln -s -f ../$DNA_FASTA
-module load mugqic/bowtie2
-bowtie2-build $DNA_FASTA $BASENAME
+echo
+echo Creating genome Picard sequence dictionary...
+echo
+INDEX_DIR=$GENOME_DIR/picard_index
+module load mugqic/picard/1.108 mugqic/java
+java -jar $PICARD_HOME/CreateSequenceDictionary.jar REFERENCE=$INDEX_DIR/$GENOME_FASTA OUTPUT=$INDEX_DIR/$GENOME_BASENAME.dict GENOME_ASSEMBLY=$GENOME_BASENAME > $LOG_DIR/picard_$TIMESTAMP.log 2>&1
+
+echo
+echo Creating genome SAMtools FASTA index...
+echo
+INDEX_DIR=$GENOME_DIR/sam_index
+module load mugqic/samtools/0.1.19
+samtools faidx $INDEX_DIR/$GENOME_FASTA > $LOG_DIR/samtools_$TIMESTAMP.log 2>&1
+
+echo
+echo Creating ncRNA BWA index...
+echo
+INDEX_DIR=$INSTALL_DIR/annotations/ncrna_bwa_index
+mkdir -p $INDEX_DIR
+ln -s -f -t $INDEX_DIR ../$NCRNA
+module load mugqic/bwa/0.7.10
+bwa index $INDEX_DIR/$NCRNA > $LOG_DIR/ncrna_bwa_$TIMESTAMP.log 2>&1
+
+BWA_CMD="INDEX_DIR=$GENOME_DIR/bwa_index && \
+module load mugqic/bwa/0.7.10 && \
+bwa index \$INDEX_DIR/$GENOME_FASTA > $LOG_DIR/bwa_$TIMESTAMP.log 2>&1"
+
+BOWTIE2_TOPHAT_CMD="
+INDEX_DIR=$GENOME_DIR/bowtie2_index && \
+module load mugqic/bowtie/2.1.0 && \
+bowtie2-build \$INDEX_DIR/$GENOME_FASTA \$INDEX_DIR/$GENOME_BASENAME > $LOG_DIR/bowtie2_$TIMESTAMP.log 2>&1 && \
+INDEX_DIR=$INSTALL_DIR/annotations/gtf_tophat_index && \
+mkdir -p \$INDEX_DIR && \
+ln -s -f -t \$INDEX_DIR ../$GTF
+module load mugqic/samtools/0.1.19 mugqic/tophat/2.0.11 && \
+tophat --output-dir \$INDEX_DIR/tophat_out --GTF \$INDEX_DIR/$GTF --transcriptome-index=\$INDEX_DIR/$BASENAME $GENOME_DIR/bowtie2_index/$GENOME_BASENAME > $LOG_DIR/gtf_tophat_$TIMESTAMP.log 2>&1"
+
+# If genome is too big, create index in a separate job since login node memory is limited
+if [ `stat --printf="%s" $GENOME_DIR/$GENOME_FASTA` -gt 200000000 ]
+then
+QSUB="qsub -m ae -M $JOB_MAIL -W umask=0002 -d $INSTALL_DIR -j oe -o $LOG_DIR/__JOB_$TIMESTAMP.log -N __JOB.$GENOME_BASENAME -l walltime=24:00:0 -q sw -l nodes=1:ppn=2"
+fi
+
+for CMD in BWA_CMD BOWTIE2_TOPHAT_CMD
+do
+# If QSUB variable is not defined, run command directly
+if [ -z "${QSUB:-}" ]
+then
+  echo
+  echo Running ${CMD}...
+  echo
+  echo "${!CMD}" | bash
+else
+  echo
+  echo Submitting ${CMD} as job...
+  echo
+  echo "${!CMD}" | ${QSUB//__JOB/$CMD}
+fi
+done
+
+# Add permissions
+chmod ug+rwX,o+rX $INSTALL_DIR
