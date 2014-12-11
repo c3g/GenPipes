b91ac7bc0d67778220825dc998aeb75decb7b67e
Joël Fillon <joel.fillon@mcgill.ca>
2014-12-11 15:25:21 -0500

    Standardized mugqic_pipelines module install script

--- resources/modules/mugqic_pipelines.sh
+++ resources/modules/mugqic_pipelines.sh
@@ -1,59 +1,93 @@
 #!/bin/bash
+# Exit immediately on error
+set -eu -o pipefail
 
 #
-# MUGQIC pipeline
+# MUGQIC Pipelines
 #
 
-SOFTWARE=mugqic_pipeline
-VERSION=1.4
-INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/$SOFTWARE
-INSTALL_DOWNLOAD=$INSTALL_PATH/tmp
-ARCHIVE=$MUGQIC_INSTALL_HOME/archive/$SOFTWARE
-mkdir -p $INSTALL_DOWNLOAD $ARCHIVE
+SOFTWARE=mugqic_pipelines
+VERSION=2.0.0
+
+# 'MUGQIC_INSTALL_HOME_DEV' for development, 'MUGQIC_INSTALL_HOME' for production (don't write '$' before!)
+INSTALL_HOME=MUGQIC_INSTALL_HOME
+
+# Indirection call to use $INSTALL_HOME value as variable name
+INSTALL_DIR=${!INSTALL_HOME}/software/$SOFTWARE
+
+# Create install directory with permissions if necessary
+if [[ ! -d $INSTALL_DIR ]]
+then
+  mkdir $INSTALL_DIR
+  chmod ug+rwX,o+rX-w $INSTALL_DIR
+fi
+
+INSTALL_DOWNLOAD=$INSTALL_DIR/tmp
+mkdir -p $INSTALL_DOWNLOAD
 cd $INSTALL_DOWNLOAD
 
 # Download, extract, build
-wget http://bitbucket.org/mugqic/$SOFTWARE/get/$VERSION.tar.gz
-tar zxvf $VERSION.tar.gz
-mv mugqic-$SOFTWARE-* v$VERSION
+# Write here the specific commands to download, extract, build the software, typically similar to:
+ARCHIVE=$SOFTWARE-$VERSION.tar.gz
+# If archive was previously downloaded, use the local one, otherwise get it from remote site
+if [[ -f ${!INSTALL_HOME}/archive/$ARCHIVE ]]
+then
+  echo "Archive $ARCHIVE already in ${!INSTALL_HOME}/archive/: using it..."
+  cp -a ${!INSTALL_HOME}/archive/$ARCHIVE .
+else
+  echo "Archive $ARCHIVE not in ${!INSTALL_HOME}/archive/: downloading it..."
+  wget https://bitbucket.org/mugqic/$SOFTWARE/downloads/$ARCHIVE --output-document=$ARCHIVE
+fi
+tar zxvf $ARCHIVE
+
+SOFTWARE_DIR=$SOFTWARE-$VERSION
 
 # Add permissions and install software
-chmod -R ug+rwX .
-chmod -R o+rX .
-mv -i v$VERSION $INSTALL_PATH
-mv -i $VERSION.tar.gz $ARCHIVE
+cd $INSTALL_DOWNLOAD
+chmod -R ug+rwX,o+rX-w .
+mv -i $SOFTWARE_DIR $INSTALL_DIR/
+# Store archive if not already present or if different from the previous one
+if [[ ! -f ${!INSTALL_HOME}/archive/$ARCHIVE || `diff ${!INSTALL_HOME}/archive/$ARCHIVE $ARCHIVE` ]]
+then
+  mv -i $ARCHIVE ${!INSTALL_HOME}/archive/
+fi
 
 # Module file
 echo "#%Module1.0
 proc ModulesHelp { } {
-       puts stderr \"\tMUGQIC - $SOFTWARE \" ;
+  puts stderr \"\tMUGQIC - $SOFTWARE \" ;
 }
-module-whatis \"$SOFTWARE  \" ;
-                      
-set             root                   \$::env(MUGQIC_INSTALL_HOME)/software/$SOFTWARE/v$VERSION ;
-setenv          MUGQIC_PIPELINE_HOME   \$root
-prepend-path    PATH                   \$root/pipelines/chipseq
-prepend-path    PATH                   \$root/pipelines/dnaseq
-prepend-path    PATH                   \$root/pipelines/rnaseq
-prepend-path    PATH                   \$root/pipelines/rnaseq_denovo
-prepend-path    PERL5LIB               \$root/lib
+module-whatis \"$SOFTWARE\" ;
+
+set             root                  \$::env($INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE_DIR
+setenv          MUGQIC_PIPELINES_HOME \$root
+prepend-path    PATH                  \$root/pipelines/dnaseq
+prepend-path    PATH                  \$root/pipelines/rnaseq
+prepend-path    PATH                  \$root/pipelines/rnaseq_denovo_assembly
+prepend-path    PATH                  \$root/pipelines/pacbio_assembly
 " > $VERSION
 
 ################################################################################
 # Everything below this line should be generic and not modified
 
-# Well... here, module directory is named "pipeline" instead of "mugqic_pipeline" for aesthetical reasons
-
 # Default module version file
 echo "#%Module1.0
 set ModulesVersion \"$VERSION\"" > .version
 
+# Set module directory path by lowercasing $INSTALL_HOME and removing '_install_home' in it
+MODULE_DIR=${!INSTALL_HOME}/modulefiles/`echo ${INSTALL_HOME,,} | sed 's/_install_home//'`/$SOFTWARE
+
+# Create module directory with permissions if necessary
+if [[ ! -d $MODULE_DIR ]]
+then
+  mkdir $MODULE_DIR
+  chmod ug+rwX,o+rX-w $MODULE_DIR
+fi
+
 # Add permissions and install module
-MODULE_DIR=$MUGQIC_INSTALL_HOME/modulefiles/mugqic/pipeline
-mkdir -p $MODULE_DIR
-chmod -R ug+rwX $VERSION .version
-chmod -R o+rX $VERSION .version
-mv $VERSION .version $MODULE_DIR
+chmod ug+rwX,o+rX-w $VERSION .version
+mv $VERSION .version $MODULE_DIR/
 
 # Clean up temporary installation files if any
+cd
 rm -rf $INSTALL_DOWNLOAD
