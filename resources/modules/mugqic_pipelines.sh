2b334e0154436b060b2af0b48804cf6537233b6d
Joël Fillon <joel.fillon@mcgill.ca>
2015-01-12 10:44:31 -0500

    Standardized mugqic_pipelines module install

--- resources/modules/mugqic_pipelines.sh
+++ resources/modules/mugqic_pipelines.sh
@@ -2,93 +2,41 @@
 # Exit immediately on error
 set -eu -o pipefail
 
-#
-# MUGQIC Pipelines
-#
-
 SOFTWARE=mugqic_pipelines
 VERSION=2.0.1
-
-# 'MUGQIC_INSTALL_HOME_DEV' for development, 'MUGQIC_INSTALL_HOME' for production (don't write '$' before!)
-INSTALL_HOME=MUGQIC_INSTALL_HOME
-
-# Indirection call to use $INSTALL_HOME value as variable name
-INSTALL_DIR=${!INSTALL_HOME}/software/$SOFTWARE
-
-# Create install directory with permissions if necessary
-if [[ ! -d $INSTALL_DIR ]]
-then
-  mkdir $INSTALL_DIR
-  chmod ug+rwX,o+rX-w $INSTALL_DIR
-fi
-
-INSTALL_DOWNLOAD=$INSTALL_DIR/tmp
-mkdir -p $INSTALL_DOWNLOAD
-cd $INSTALL_DOWNLOAD
-
-# Download, extract, build
-# Write here the specific commands to download, extract, build the software, typically similar to:
 ARCHIVE=$SOFTWARE-$VERSION.tar.gz
-# If archive was previously downloaded, use the local one, otherwise get it from remote site
-if [[ -f ${!INSTALL_HOME}/archive/$ARCHIVE ]]
-then
-  echo "Archive $ARCHIVE already in ${!INSTALL_HOME}/archive/: using it..."
-  cp -a ${!INSTALL_HOME}/archive/$ARCHIVE .
-else
-  echo "Archive $ARCHIVE not in ${!INSTALL_HOME}/archive/: downloading it..."
-  wget https://bitbucket.org/mugqic/$SOFTWARE/downloads/$ARCHIVE --output-document=$ARCHIVE
-fi
-tar zxvf $ARCHIVE
-
+ARCHIVE_URL=https://bitbucket.org/mugqic/$SOFTWARE/downloads/$ARCHIVE
 SOFTWARE_DIR=$SOFTWARE-$VERSION
 
-# Add permissions and install software
-cd $INSTALL_DOWNLOAD
-chmod -R ug+rwX,o+rX-w .
-mv -i $SOFTWARE_DIR $INSTALL_DIR/
-# Store archive if not already present or if different from the previous one
-if [[ ! -f ${!INSTALL_HOME}/archive/$ARCHIVE || `diff ${!INSTALL_HOME}/archive/$ARCHIVE $ARCHIVE` ]]
-then
-  mv -i $ARCHIVE ${!INSTALL_HOME}/archive/
-fi
+# Specific commands to extract and build the software
+# $INSTALL_DIR and $INSTALL_DOWNLOAD have been set automatically
+# $ARCHIVE has been downloaded in $INSTALL_DOWNLOAD
+build() {
+  cd $INSTALL_DOWNLOAD
+  tar zxvf $ARCHIVE
 
-# Module file
-echo "#%Module1.0
-proc ModulesHelp { } {
-  puts stderr \"\tMUGQIC - $SOFTWARE \" ;
+  # Install software
+  mv -i $SOFTWARE_DIR $INSTALL_DIR/
 }
-module-whatis \"$SOFTWARE\" ;
 
-set             root                  \$::env($INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE_DIR
+module_file() {
+echo "\
+#%Module1.0
+proc ModulesHelp { } {
+  puts stderr \"\tMUGQIC - $SOFTWARE \"
+}
+module-whatis \"$SOFTWARE\"
+
+set             root                   $INSTALL_DIR/$SOFTWARE_DIR
 setenv          MUGQIC_PIPELINES_HOME \$root
 prepend-path    PATH                  \$root/utils
 prepend-path    PATH                  \$root/pipelines/dnaseq
 prepend-path    PATH                  \$root/pipelines/rnaseq
 prepend-path    PATH                  \$root/pipelines/rnaseq_denovo_assembly
 prepend-path    PATH                  \$root/pipelines/pacbio_assembly
-" > $VERSION
+"
+}
 
-################################################################################
-# Everything below this line should be generic and not modified
-
-# Default module version file
-echo "#%Module1.0
-set ModulesVersion \"$VERSION\"" > .version
-
-# Set module directory path by lowercasing $INSTALL_HOME and removing '_install_home' in it
-MODULE_DIR=${!INSTALL_HOME}/modulefiles/`echo ${INSTALL_HOME,,} | sed 's/_install_home//'`/$SOFTWARE
-
-# Create module directory with permissions if necessary
-if [[ ! -d $MODULE_DIR ]]
-then
-  mkdir $MODULE_DIR
-  chmod ug+rwX,o+rX-w $MODULE_DIR
-fi
-
-# Add permissions and install module
-chmod ug+rwX,o+rX-w $VERSION .version
-mv $VERSION .version $MODULE_DIR/
-
-# Clean up temporary installation files if any
-cd
-rm -rf $INSTALL_DOWNLOAD
+# Call generic module install script once all variables and functions have been set
+MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $MODULE_INSTALL_SCRIPT_DIR/install_module.sh $@
