a2c58248633bac01d60d4ecc27951736ccea33ae
Joël Fillon <joel.fillon@mcgill.ca>
2015-01-12 11:22:06 -0500

    More standardization of module install

--- resources/modules/bowtie.sh
+++ resources/modules/bowtie.sh
@@ -8,10 +8,6 @@ ARCHIVE=$SOFTWARE-$VERSION-src.zip
 ARCHIVE_URL=http://sourceforge.net/projects/bowtie-bio/files/$SOFTWARE/$VERSION/$ARCHIVE
 SOFTWARE_DIR=$SOFTWARE-$VERSION
 
-# 'MUGQIC_INSTALL_HOME_DEV' for development, 'MUGQIC_INSTALL_HOME' for production (don't write '$' before!)
-# 'MUGQIC_INSTALL_HOME' must be explicitely passed as first parameter, otherwise 'MUGQIC_INSTALL_HOME_DEV' is used
-INSTALL_HOME=${1:-MUGQIC_INSTALL_HOME_DEV}
-
 # Specific commands to extract and build the software
 # $INSTALL_DIR and $INSTALL_DOWNLOAD have been set automatically
 # $ARCHIVE has been downloaded in $INSTALL_DOWNLOAD
@@ -27,15 +23,15 @@ build() {
   mv -i $SOFTWARE_DIR $INSTALL_DIR/
 }
 
-# Module file
-MODULE_FILE="\
+module_file() {
+echo "\
 #%Module1.0
 proc ModulesHelp { } {
   puts stderr \"\tMUGQIC - $SOFTWARE \"
 }
 module-whatis \"$SOFTWARE\"
 
-set             root                \$::env($INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE_DIR
+set             root                $INSTALL_DIR/$SOFTWARE_DIR
 prepend-path    PATH                \$root
 "
 
