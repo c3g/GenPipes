07bba1e88068217c420ee6119e43f97f96a9c563
Joël Fillon <joel.fillon@mcgill.ca>
2015-01-15 17:51:32 -0500

    BFXDEV-35 Fixed path bugs in MUMmer and AMOS module install

--- resources/modules/dev/amos.sh
+++ resources/modules/dev/amos.sh
@@ -2,91 +2,56 @@
 # Exit immediately on error
 set -eu -o pipefail
 
-#
-# AMOS
-#
-
 SOFTWARE=amos
 VERSION=3.1.0
-
-# 'MUGQIC_INSTALL_HOME_DEV' for development, 'MUGQIC_INSTALL_HOME' for production (don't write '$' before!)
-INSTALL_HOME=MUGQIC_INSTALL_HOME_DEV
-
-# Indirection call to use $INSTALL_HOME value as variable name
-INSTALL_DIR=${!INSTALL_HOME}/software/$SOFTWARE
-
-# Create install directory with permissions if necessary
-if [[ ! -d $INSTALL_DIR ]]
-then
-  mkdir $INSTALL_DIR
-  chmod ug+rwX,o+rX $INSTALL_DIR
-fi
-
-INSTALL_DOWNLOAD=$INSTALL_DIR/tmp
-mkdir -p $INSTALL_DOWNLOAD
-cd $INSTALL_DOWNLOAD
-
-# Download, extract, build
 ARCHIVE=$SOFTWARE-$VERSION.tar.gz
-# If archive was previously downloaded, use the local one, otherwise get it from remote site
-if [[ -f ${!INSTALL_HOME}/archive/$ARCHIVE ]]
-then
-  echo "Archive $ARCHIVE already in ${!INSTALL_HOME}/archive/: using it..."
-  cp -a ${!INSTALL_HOME}/archive/$ARCHIVE .
-else
-  echo "Archive $ARCHIVE not in ${!INSTALL_HOME}/archive/: downloading it..."
-  wget http://downloads.sourceforge.net/project/$SOFTWARE/$SOFTWARE/$VERSION/$ARCHIVE
-fi
-tar zxvf $ARCHIVE
-
+ARCHIVE_URL=http://downloads.sourceforge.net/project/$SOFTWARE/$SOFTWARE/$VERSION/$ARCHIVE
 SOFTWARE_DIR=$SOFTWARE-$VERSION
-cd $SOFTWARE_DIR
-# With-qmake seems to work on guillimin and mammouth as well as abacus
-module load mugqic/MUMmer mugqic/ucsc
-./configure --prefix=$INSTALL_DIR/$SOFTWARE_DIR --with-qmake-qt4=/usr/lib64/qt4/bin/qmake
-make
-make install
 
-# Add permissions
-cd $INSTALL_DOWNLOAD
-chmod -R ug+rwX,o+rX . $INSTALL_DIR/$SOFTWARE_DIR
-# Store archive if not already present or if different from the previous one
-if [[ ! -f ${!INSTALL_HOME}/archive/$ARCHIVE || `diff ${!INSTALL_HOME}/archive/$ARCHIVE $ARCHIVE` ]]
-then
-  mv -i $ARCHIVE ${!INSTALL_HOME}/archive/
-fi
+# Specific commands to extract and build the software
+# $INSTALL_DIR and $INSTALL_DOWNLOAD have been set automatically
+# $ARCHIVE has been downloaded in $INSTALL_DOWNLOAD
+build() {
+  cd $INSTALL_DOWNLOAD
+  tar zxvf $ARCHIVE
 
-# Module file
-echo "#%Module1.0
+  cd $SOFTWARE_DIR
+  # AMOS hard-codes dependency paths
+  module load mugqic/perl/5.18.2 mugqic/python/2.7.8 mugqic/MUMmer/3.23 mugqic/ucsc/20140212
+  ./configure \
+    PERL=`which perl` \
+    PYTHON=`which python` \
+    NUCMER=`which nucmer` \
+    DELTAFILTER=`which delta-filter` \
+    SHOWCOORDS=`which show-coords` \
+    BLAT=`which blat` \
+    --prefix=$INSTALL_DIR/$SOFTWARE_DIR \
+    --with-qmake-qt4=/usr/lib64/qt4/bin/qmake
+  make
+  make install
+
+  # DELTAFILTER and SHOWCOORDS are ignored by ./configure for minimus2 (bug?)
+  perl -pi -e s,"DELTAFILTER\s+=.*,DELTAFILTER=`which delta-filter`,g" $INSTALL_DIR/$SOFTWARE_DIR/bin/minimus2
+  perl -pi -e s,"SHOWCOORDS\s+=.*,SHOWCOORDS=`which show-coords`,g" $INSTALL_DIR/$SOFTWARE_DIR/bin/minimus2
+
+  # Update Python script shebang
+  #sed -i s,"#\!/usr/bin/perl,#\!/usr/bin/env perl,g" $INSTALL_DIR/$SOFTWARE_DIR/bin/*
+}
+
+module_file() {
+echo "\
+#%Module1.0
 proc ModulesHelp { } {
   puts stderr \"\tMUGQIC - $SOFTWARE \"
 }
 module-whatis \"$SOFTWARE\"
 
-set             root                \$::env($INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE_DIR
+set             root                $INSTALL_DIR/$SOFTWARE_DIR
 prepend-path    PATH                \$root/bin
-" > $VERSION
+"
+}
 
-################################################################################
-# Everything below this line should be generic and not modified
-
-# Default module version file
-echo "#%Module1.0
-set ModulesVersion \"$VERSION\"" > .version
-
-# Set module directory path by removing '_INSTALL_HOME' in $INSTALL_HOME and lowercasing the result
-MODULE_DIR=${!INSTALL_HOME}/modulefiles/`echo ${INSTALL_HOME/_INSTALL_HOME/} | tr '[:upper:]' '[:lower:]'`/$SOFTWARE
-
-# Create module directory with permissions if necessary
-if [[ ! -d $MODULE_DIR ]]
-then
-  mkdir $MODULE_DIR
-  chmod ug+rwX,o+rX $MODULE_DIR
-fi
-
-# Add permissions and install module
-chmod ug+rwX,o+rX $VERSION .version
-mv $VERSION .version $MODULE_DIR/
-
-# Clean up temporary installation files if any
-rm -rf $INSTALL_DOWNLOAD
+# Call generic module install script once all variables and functions have been set
+MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# IMPORTANT: to install amos in production, remove '../' before install_module.sh and move this script in parent dir
+source $MODULE_INSTALL_SCRIPT_DIR/../install_module.sh $@
