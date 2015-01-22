284138b55998fa8896546989f9a7abc593fd6a8c
Joël Fillon <joel.fillon@mcgill.ca>
2015-01-22 17:57:02 -0500

    BFXDEV-35 Standardized trinotate module install

--- resources/modules/trinotate.sh
+++ resources/modules/trinotate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-#
-# Trinotate
-#
+# Exit immediately on error
+set -eu -o pipefail
 
 # NOTE: 
 # - Perl module DB_File is a dependency for the Transdecoder part of Trinotate. This module depends on some version BerkeleyDB which was not present on Mammouth...
@@ -13,88 +11,43 @@
 
 SOFTWARE=trinotate
 VERSION=20131110
+ARCHIVE=${SOFTWARE^}_r$VERSION.tar.gz
+ARCHIVE_URL=http://downloads.sourceforge.net/project/$SOFTWARE/$ARCHIVE
+SOFTWARE_DIR=${SOFTWARE^}_r$VERSION
 
-# 'MUGQIC_INSTALL_HOME_DEV' for development, 'MUGQIC_INSTALL_HOME' for production (don't write '$' before!)
-INSTALL_HOME=MUGQIC_INSTALL_HOME
+# Specific commands to extract and build the software
+# $INSTALL_DIR and $INSTALL_DOWNLOAD have been set automatically
+# $ARCHIVE has been downloaded in $INSTALL_DOWNLOAD
+build() {
+  cd $INSTALL_DOWNLOAD
+  tar zxvf $ARCHIVE
 
-# Indirection call to use $INSTALL_HOME value as variable name
-INSTALL_DIR=${!INSTALL_HOME}/software/$SOFTWARE
+  # Download Trinotate Sqlite template DB
+  SQLITE_ARCHIVE=Trinotate.sqlite.gz
+  download_archive "http://sourceforge.net/projects/trinotate/files/TRINOTATE_RESOURCES/TrinotateSqlite.sprot.$VERSION.db.gz/download" $SQLITE_ARCHIVE
+  gunzip $SQLITE_ARCHIVE -c > $SOFTWARE_DIR/${SQLITE_ARCHIVE/.gz/}
 
-# Create install directory with permissions if necessary
-if [[ ! -d $INSTALL_DIR ]]
-then
-  mkdir $INSTALL_DIR
-  chmod ug+rwX,o+rX $INSTALL_DIR
-fi
+  # Install software
+  mv -i $SOFTWARE_DIR $INSTALL_DIR/
 
-INSTALL_DOWNLOAD=$INSTALL_DIR/tmp
-mkdir $INSTALL_DOWNLOAD
-cd $INSTALL_DOWNLOAD
+  store_archive $SQLITE_ARCHIVE
+}
 
-# Download, extract, build
-# Write here the specific commands to download, extract, build the software, typically similar to:
-ARCHIVE=Trinotate_r$VERSION.tar.gz
-# If archive was previously downloaded, use the local one, otherwise get it from remote site
-if [[ -f ${!INSTALL_HOME}/archive/$ARCHIVE ]]
-then
-  echo "Archive $ARCHIVE already in ${!INSTALL_HOME}/archive/: using it..."
-  cp -a ${!INSTALL_HOME}/archive/$ARCHIVE .
-else
-  echo "Archive $ARCHIVE not in ${!INSTALL_HOME}/archive/: downloading it..."
-  wget http://downloads.sourceforge.net/project/$SOFTWARE/$ARCHIVE
-fi
-tar zxvf $ARCHIVE
-
-SOFTWARE_DIR=Trinotate_r$VERSION
-cd $SOFTWARE_DIR
-# Download Trinotate Sqlite template DB
-wget "http://sourceforge.net/projects/trinotate/files/TRINOTATE_RESOURCES/TrinotateSqlite.sprot.$VERSION.db.gz/download" -O Trinotate.sqlite.gz
-gunzip Trinotate.sqlite.gz
-
-# Add permissions and install software
-cd $INSTALL_DOWNLOAD
-chmod -R ug+rwX,o+rX .
-mv -i $SOFTWARE_DIR $INSTALL_DIR
-# Store archive if not already present or if different from the previous one
-if [[ ! -f ${!INSTALL_HOME}/archive/$ARCHIVE || `diff ${!INSTALL_HOME}/archive/$ARCHIVE $ARCHIVE` ]]
-then
-  mv -i $ARCHIVE ${!INSTALL_HOME}/archive/
-fi
-
-# Module file
-echo "#%Module1.0
+module_file() {
+echo "\
+#%Module1.0
 proc ModulesHelp { } {
   puts stderr \"\tMUGQIC - $SOFTWARE \"
 }
 module-whatis \"$SOFTWARE\"
 
-prereq                              mugqic/trinity
-set             root                \$::env($INSTALL_HOME)/software/$SOFTWARE/$SOFTWARE_DIR
+set             root                $INSTALL_DIR/$SOFTWARE_DIR
 prepend-path    PATH                \$root
 setenv          TRINOTATE_HOME      \$root
 setenv          TRINOTATE_SQLITE    \$root/Trinotate.sqlite
-" > $VERSION
+"
+}
 
-################################################################################
-# Everything below this line should be generic and not modified
-
-# Default module version file
-echo "#%Module1.0
-set ModulesVersion \"$VERSION\"" > .version
-
-# Set module directory path by removing '_INSTALL_HOME' in $INSTALL_HOME and lowercasing the result
-MODULE_DIR=${!INSTALL_HOME}/modulefiles/`echo ${INSTALL_HOME/_INSTALL_HOME/} | tr '[:upper:]' '[:lower:]'`/$SOFTWARE
-
-# Create module directory with permissions if necessary
-if [[ ! -d $MODULE_DIR ]]
-then
-  mkdir $MODULE_DIR
-  chmod ug+rwX,o+rX $MODULE_DIR
-fi
-
-# Add permissions and install module
-chmod ug+rwX,o+rX $VERSION .version
-mv $VERSION .version $MODULE_DIR
-
-# Clean up temporary installation files if any
-rm -rf $INSTALL_DOWNLOAD
+# Call generic module install script once all variables and functions have been set
+MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $MODULE_INSTALL_SCRIPT_DIR/install_module.sh $@
