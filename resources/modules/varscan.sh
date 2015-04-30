fffb12b62407cda09d60d4c8d83daba6a1932b95
lletourn <louis.letourneau@mail.mcgill.ca>
2015-04-30 15:39:35 -0400

    BFXDEV-392 need varscan for high coverage

---
+++ resources/modules/varscan.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Exit immediately on error
+set -eu -o pipefail
+
+SOFTWARE=VarScan
+VERSION=2.3.8
+# Replace "." in official version number by "_" in archive version number
+ARCHIVE=${SOFTWARE}.v${VERSION}.jar
+ARCHIVE_URL=http://sourceforge.net/projects/varscan/files/$ARCHIVE
+SOFTWARE_DIR=${SOFTWARE}.v${VERSION}
+
+# Specific commands to extract and build the software
+# $INSTALL_DIR and $INSTALL_DOWNLOAD have been set automatically
+# $ARCHIVE has been downloaded in $INSTALL_DOWNLOAD
+build() {
+  cd $INSTALL_DOWNLOAD
+
+  # Install software
+  mkdir -p $INSTALL_DIR/$SOFTWARE_DIR
+  cp -i $ARCHIVE $INSTALL_DIR/$SOFTWARE_DIR/
+}
+
+module_file() {
+echo "\
+#%Module1.0
+proc ModulesHelp { } {
+  puts stderr \"\tMUGQIC - $SOFTWARE \"
+}
+module-whatis \"$SOFTWARE\"
+
+set             root                $INSTALL_DIR/$SOFTWARE_DIR
+setenv          VARSCAN_HOME        \$root
+setenv          VARSCAN_JAR         \$root/${SOFTWARE}.v${VERSION}.jar
+"
+}
+
+# Call generic module install script once all variables and functions have been set
+MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $MODULE_INSTALL_SCRIPT_DIR/install_module.sh $@
