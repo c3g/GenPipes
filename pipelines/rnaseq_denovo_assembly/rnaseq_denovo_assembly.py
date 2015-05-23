126102e7c8312ce810c751a7ff47c38ca62483b9
Johanna Sandoval <johanna.sandoval@mail.mcgill.ca>
2015-05-22 18:15:26 -0700

    detected bugs during tests

--- pipelines/rnaseq_denovo_assembly/rnaseq_denovo_assembly.py
+++ pipelines/rnaseq_denovo_assembly/rnaseq_denovo_assembly.py
@@ -660,9 +660,16 @@ tmhmm --short \\
         tmhmm = os.path.join("trinotate", "tmhmm", "tmhmm.out")
         trinotate_sqlite = os.path.join("trinotate", "Trinotate.sqlite")
         trinotate_report = os.path.join("trinotate", "trinotate_annotation_report.tsv")
-		trinotate_report_genes_prefix = os.path.join("trinotate", "trinotate_annotation_report.tsv.genes")
-		trinotate_report_transcripts_prefix = os.path.join("trinotate", "trinotate_annotation_report.tsv.transcripts")
-
+        trinotate_report_genes_prefix = os.path.join("trinotate", "trinotate_annotation_report.tsv.genes")
+        trinotate_report_transcripts_prefix = os.path.join("trinotate", "trinotate_annotation_report.tsv.transcripts")
+        if not config.param('trinotate', 'gene_column'):
+            gene_id_column="#gene_id" 
+        else:
+            gene_id_column=config.param('trinotate', 'gene_column')
+        if not config.param('trinotate', 'transcript_column'):
+            transcript_id_column="transcript_id"
+        else: 
+            transcript_id_column=config.param('trinotate', 'transcript_column')
         return [concat_jobs([
             Job(command="mkdir -p trinotate"),
             Job(
@@ -706,21 +713,21 @@ Trinotate {trinotate_sqlite} report -E {evalue} --pfam_cutoff {pfam_cutoff} \\
                     pfam_cutoff=config.param('trinotate', 'pfam_cutoff'),
                     trinotate_report=trinotate_report
             )),
-			Job(
-			[trinotate_report], 
-			[trinotate_report_genes_prefix + '_blast.tsv', trinotate_report_transcripts_prefix + '_blast.tsv' , 
-			trinotate_report_genes_prefix + '_go.tsv', trinotate_report_transcripts_prefix + '_go.tsv' ],
-			['trinotate', 'module_mugqic_tools'], 
-			command="""\
+            Job(
+            [trinotate_report], 
+            [trinotate_report_genes_prefix + '_blast.tsv', trinotate_report_transcripts_prefix + '_blast.tsv' , 
+            trinotate_report_genes_prefix + '_go.tsv', trinotate_report_transcripts_prefix + '_go.tsv' ],
+            [['trinotate', 'module_mugqic_tools']], 
+            command="""\
 $PYTHON_TOOLS/parseTrinotateOutput.py -r {trinotate_report} -o {trinotate_report_gene_prefix} -i {gene_id_column} &&
 $PYTHON_TOOLS/parseTrinotateOutput.py -r {trinotate_report} -o {trinotate_report_transcript_prefix} -i {transcript_id_column}
-			""".format(
-			trinotate_report=trinotate_report,
-			trinotate_report_gene_prefix=trinotate_report_gene_prefix,
-			trinotate_report_transcript_prefix=trinotate_report_transcript_prefix,
-			gene_id_column= "#gene_id" if not config.param('trinotate', 'gene_column'),
-			transcript_id_column="transcript_id" if not config.param('trinotate', 'transcript_column')
-			))
+            """.format(
+            trinotate_report=trinotate_report,
+            trinotate_report_gene_prefix=trinotate_report_gene_prefix,
+            trinotate_report_transcript_prefix=trinotate_report_transcript_prefix,
+            gene_id_column = transcript_id_column,
+            transcript_id_column=transcript_id_column
+            ))
     ], name="trinotate")]
 
     def align_and_estimate_abundance_prep_reference(self):
@@ -1003,8 +1010,8 @@ done""".format(
                 os.path.join("differential_expression", "genes" , contrast.name, "dge_trinotate_results.csv"),
                 config.param("differential_expression_goseq", "dge_input_columns"),
                 os.path.join("differential_expression", "genes", contrast.name ,"gene_ontology_go_results.csv"),
-                os.path.join("differential_expression", "genes.lengths.tsv")
-		os.path.join("trinotate", "trinotate_annotation_report.tsv.genes_go.tsv")
+                os.path.join("differential_expression", "genes.lengths.tsv"),
+                os.path.join("trinotate", "trinotate_annotation_report.tsv.genes_go.tsv")
             )
             job.name = "differential_expression_goseq.dge." + contrast.name
             jobs.append(job)
