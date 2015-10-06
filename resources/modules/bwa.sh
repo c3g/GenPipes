7769b771d3a78a4b7221be3007c849962f0118d7
mathieu bourgey <mathieu.bourgey@mail.mcgill.ca>
2015-10-06 15:38:18 -0400

    update bwa install script to the newer version

--- resources/modules/bwa.sh
+++ resources/modules/bwa.sh
@@ -3,7 +3,7 @@
 set -eu -o pipefail
 
 SOFTWARE=bwa
-VERSION=0.7.10
+VERSION=0.7.12
 ARCHIVE=$SOFTWARE-$VERSION.tar.bz2
 ARCHIVE_URL=http://downloads.sourceforge.net/project/bio-bwa/$ARCHIVE
 SOFTWARE_DIR=$SOFTWARE-$VERSION
