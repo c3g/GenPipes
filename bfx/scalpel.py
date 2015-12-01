5ed57839e221ed42ed3575815108bb915979dd1c
Mathieu Bourgey <mathieu.bourgey@mail.mcgill.ca>
2015-12-01 09:17:38 -0500

    tumor_pair - add the two pass option to scalpel - BFXDEV-478

--- bfx/scalpel.py
+++ bfx/scalpel.py
@@ -42,12 +42,13 @@ scalpel --somatic \\
   --tumor {inputTumor} \\
   --dir {outputDir} \\
   --numprocs {cores_per_job} \\
-  --bed {bed}""".format(
+  --bed {bed}{options}""".format(
         reference_sequence=config.param('scalpel', 'genome_fasta', type='filepath'),
         inputNormal=inputNormal,
         inputTumor=inputTumor,
         outputDir=outputDir,
         cores_per_job=config.param('scalpel', 'cores_per_job'),
         bed=bed
+        options=config.param('scalpel', 'options', type='string')
         )
     )
