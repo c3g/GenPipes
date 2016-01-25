ec80af334a0c9a21aff0b6d6cf7d90dd5dca0d7d
Edouard Henrion <edouard.henrion@mcgill.ca>
2016-01-25 11:46:35 -0500

    modules - updated vcftools VERSION in vcftools.sh - BFXDEV-490

--- resources/modules/vcftools.sh
+++ resources/modules/vcftools.sh
@@ -3,6 +3,7 @@
 set -eu -o pipefail
 
 SOFTWARE=vcftools
+#VERSION=0.1.13
 VERSION=0.1.14
 ARCHIVE=${SOFTWARE}-$VERSION.tar.gz
 ARCHIVE_URL=https://github.com/$SOFTWARE/$SOFTWARE/releases/download/v${VERSION}/$ARCHIVE
@@ -26,6 +27,22 @@ build() {
   mv -i $SOFTWARE_DIR $INSTALL_DIR/
 }
 
+#Module definition to use for version 0.1.13
+#module_file() {
+#echo "\
+##%Module1.0
+#proc ModulesHelp { } {
+#  puts stderr \"\tMUGQIC - $SOFTWARE \"
+#}
+#module-whatis \"$SOFTWARE\"
+#
+#set             root                $INSTALL_DIR/$SOFTWARE_DIR
+#prepend-path    PATH                \$root/bin
+#prepend-path    PERL5LIB            \$root/lib/perl5/site_perl
+#"
+#}
+
+#Module definition to use for version 0.1.14
 module_file() {
 echo "\
 #%Module1.0
@@ -36,9 +53,10 @@ module-whatis \"$SOFTWARE\"
 
 set             root                $INSTALL_DIR/$SOFTWARE_DIR
 prepend-path    PATH                \$root/bin
-prepend-path    PERL5LIB            \$root/lib/perl5/site_perl
+prepend-path    PERL5LIB            \$root/share/perl5
 "
 }
+
 
 # Call generic module install script once all variables and functions have been set
 MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
