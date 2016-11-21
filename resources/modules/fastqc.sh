f4a2adb515d6179ff510e2a9dbf76bea6ac5bbb3
Edouard Henrion <edouard.henrion@mcgill.ca>
2016-11-21 15:57:16 -0500

    FASTQC - updated installation script with newer version and integration of /lb/project/mugqic/analyste_dev as a possible installation path

--- resources/modules/fastqc.sh
+++ resources/modules/fastqc.sh
@@ -4,7 +4,17 @@
 ################### FASTQC
 ###################
 VERSION="0.11.2"
-INSTALL_PATH=$MUGQIC_INSTALL_HOME/software/fastqc/fastqc_v"$VERSION" # where to install..
+if [[ ${1:-} == MUGQIC_INSTALL_HOME ]]
+then
+  INSTALL_HOME=MUGQIC_INSTALL_HOME
+  MODULE=mugqic
+else
+  INSTALL_HOME=MUGQIC_INSTALL_HOME_DEV
+  MODULE=mugqic_dev
+fi
+
+INSTALL_PATH=${!INSTALL_HOME}/software/fastqc/fastqc_v"$VERSION" # where to install...
+
 mkdir -p $INSTALL_PATH
 cd $INSTALL_PATH
 wget http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v"$VERSION".zip
@@ -12,6 +22,7 @@ unzip fastqc_v"$VERSION".zip
 rm fastqc_v"$VERSION".zip
 chmod +x FastQC/fastqc
 chmod -R g+w $INSTALL_PATH
+sed -i s,"#\!/usr/bin/perl.*,#\!/usr/bin/env perl,g" FastQC/fastqc
 
 # Module file
 echo "#%Module1.0
@@ -20,8 +31,8 @@ proc ModulesHelp { } {
 }
 module-whatis \"MUGQIC -FASTQC \"
                       
-set             root                \$::env(MUGQIC_INSTALL_HOME)/software/fastqc/fastqc_v"$VERSION"/FastQC
-prepend-path    PATH               \$root
+set             root                $INSTALL_PATH/FastQC
+prepend-path    PATH                \$root
 " > $VERSION
 
 # Version file
@@ -29,7 +40,7 @@ echo "#%Module1.0
 set ModulesVersion \"$VERSION\"
 " > .version
 
-mkdir -p $MUGQIC_INSTALL_HOME/modulefiles/mugqic/fastqc
-mv .version $VERSION $MUGQIC_INSTALL_HOME/modulefiles/mugqic/fastqc/
+mkdir -p ${!INSTALL_HOME}/modulefiles/$MODULE/fastqc
+mv .version $VERSION ${!INSTALL_HOME}/modulefiles/$MODULE/fastqc/
 
 
