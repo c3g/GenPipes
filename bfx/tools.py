29e74ac587d67c6757446679b6ba4094c554527b
Edouard Henrion <edouard.henrion@mcgill.ca>
2017-11-16 13:57:49 -0500

    minor updates on bfx/tools.py especially to make indentation uniform across the whole file

--- bfx/tools.py
+++ bfx/tools.py
@@ -41,8 +41,8 @@ def py_addLengthRay (file_scaffolds_fasta, length_file, output):
 python $PYTHON_TOOLS/addLengthRay.py \\
   -s {scaFile} \\
   -l {lenFile}""".format(
-        scaFile=file_scaffolds_fasta,
-        lenFile=length_file
+            scaFile=file_scaffolds_fasta,
+            lenFile=length_file
         )
     )
 
@@ -58,8 +58,8 @@ def py_blastMatchSca (prefix_scaffolds_fasta, blast_file, output):
 python $PYTHON_TOOLS/blastMatchSca.py \\
   -f {scaFile} \\
   -b {blastFile}""".format(
-        scaFile=prefix_scaffolds_fasta,
-        blastFile=blast_file
+            scaFile=prefix_scaffolds_fasta,
+            blastFile=blast_file
         )
     )
 
@@ -75,8 +75,8 @@ def py_equalFastqFile (fastq_ref, fastq, output):
 python $PYTHON_TOOLS/equalFastqFile.py \\
   -r {ref} \\
   -f {fastq}""".format(
-        ref=fastq_ref,
-        fastq=fastq
+            ref=fastq_ref,
+            fastq=fastq
         )
     )
 
@@ -94,10 +94,10 @@ python $PYTHON_TOOLS/rrnaBAMcounter.py \\
   -g {gtf} \\
   -o {output} \\
   -t {typ}""".format(
-        bam=bam,
-        gtf=gtf,
-        output=output,
-        typ=typ
+            bam=bam,
+            gtf=gtf,
+            output=output,
+            typ=typ
         )
     )
 # Parse Trinotate output, create best blast annotated file, GO terms and a list of filtered configs
@@ -107,20 +107,21 @@ def py_parseTrinotateOutput(trinotate_annotation_report, trinotate_report_genes_
         [trinotate_report_genes_prefix + '_blast.tsv', trinotate_report_transcripts_prefix + '_blast.tsv' ,
         trinotate_report_genes_prefix + '_go.tsv', trinotate_report_transcripts_prefix + '_go.tsv',
         trinotate_report_transcripts_prefix + '_filtered.tsv'],
-        [['DEFAULT', 'module_mugqic_tools'],
-         ['DEFAULT', 'module_python']
-         ],
+        [
+            ['DEFAULT', 'module_mugqic_tools'],
+            ['DEFAULT', 'module_python']
+        ],
         name=job_name,
         command="""\
 $PYTHON_TOOLS/parseTrinotateOutput.py -r {trinotate_annotation_report} -o {trinotate_report_genes_prefix} -i \"{gene_id_column}\" -l {isoforms_lengths} &&
 $PYTHON_TOOLS/parseTrinotateOutput.py -r {trinotate_annotation_report} -o {trinotate_report_transcripts_prefix} -i \"{transcript_id_column}\"{filters}""".format(
-        trinotate_annotation_report=trinotate_annotation_report,
-        trinotate_report_genes_prefix=trinotate_report_genes_prefix,
-        trinotate_report_transcripts_prefix=trinotate_report_transcripts_prefix,
-        gene_id_column = gene_id_column,
-        isoforms_lengths=isoforms_lengths_file,
-        transcript_id_column=transcript_id_column,
-        filters="" if not filters else " -f " + ' and '.join(filters)
+            trinotate_annotation_report=trinotate_annotation_report,
+            trinotate_report_genes_prefix=trinotate_report_genes_prefix,
+            trinotate_report_transcripts_prefix=trinotate_report_transcripts_prefix,
+            gene_id_column=gene_id_column,
+            isoforms_lengths=isoforms_lengths_file,
+            transcript_id_column=transcript_id_column,
+            filters="" if not filters else " -f " + ' and '.join(filters)
         )
     )
 
@@ -128,24 +129,25 @@ def py_parseMergeCsv(input_files, delimiter, output , common, subset=None, exclu
     return Job(
         input_files,
         [output],
-        [['DEFAULT', 'module_mugqic_tools'],
-         ['DEFAULT', 'module_python']
-         ],
+        [
+            ['DEFAULT', 'module_mugqic_tools'],
+            ['DEFAULT', 'module_python']
+        ],
         command="""\
 $PYTHON_TOOLS/parseMergeCsv.py -i {input_files} \\
       -o {output} \\
       -c {common_columns} \\
       -d {delimiter} {subset}{toexclude}{left_outer_join}{sort_by_field}{make_names}{filters}""".format(
-        input_files=" ".join(input_files),
-        output=output,
-        common_columns=common,
-        delimiter=delimiter,
-        subset=" -s " + subset if subset else "",
-        toexclude=" -x " + exclude if exclude else "",
-        left_outer_join=" -l " if  left_join else "",
-        sort_by_field=" -t " + sort_by if sort_by else "",
-        make_names=" -n " if make_names else "",
-        filters="" if not filters else " -f " + ' and '.join(filters)
+            input_files=" ".join(input_files),
+            output=output,
+            common_columns=common,
+            delimiter=delimiter,
+            subset=" -s " + subset if subset else "",
+            toexclude=" -x " + exclude if exclude else "",
+            left_outer_join=" -l " if  left_join else "",
+            sort_by_field=" -t " + sort_by if sort_by else "",
+            make_names=" -n " if make_names else "",
+            filters="" if not filters else " -f " + ' and '.join(filters)
         )
     )
 
@@ -153,7 +155,7 @@ def py_ampliconSeq(input_files, output_files, function, supplemental_parameters)
     return Job(
         input_files,
         output_files,
-        module_entries=[
+        [
             ['DEFAULT', 'module_mugqic_tools'],
             ['DEFAULT', 'module_python']
         ],
@@ -161,8 +163,8 @@ def py_ampliconSeq(input_files, output_files, function, supplemental_parameters)
 python $PYTHON_TOOLS/AmpliconSeq_script.py \\
   -m {function} \\
   {supplemental_parameters}""".format(
-        function=function,
-        supplemental_parameters=supplemental_parameters
+            function=function,
+            supplemental_parameters=supplemental_parameters
         )
     )
 
@@ -170,18 +172,19 @@ def py_filterAssemblyToFastaToTsv(fasta_file, filter_file, fasta_id_column, outp
     return Job(
         [ fasta_file , filter_file],
         [ output + "." + ext for ext in ["fasta", "tsv"] ],
-        [['DEFAULT', 'module_mugqic_tools'],
-         ['DEFAULT', 'module_python']
-         ],
+        [
+            ['DEFAULT', 'module_mugqic_tools'],
+            ['DEFAULT', 'module_python']
+        ],
         command="""\
 $PYTHON_TOOLS/filterAssemblyToFastaToXls.py -f {fasta_file} \\
 -o {output} \\
 -l {filter_file} \\
 -c {fasta_id_column} """.format(
-        fasta_file = fasta_file,
-        output=output,
-        filter_file=filter_file,
-        fasta_id_column=fasta_id_column
+            fasta_file=fasta_file,
+            output=output,
+            filter_file=filter_file,
+            fasta_id_column=fasta_id_column
         )
     )
 
@@ -197,8 +200,8 @@ def dict2beds(dictionary,beds):
 dict2BEDs.py \\
   --dict {dictionary} \\
   --beds {beds}""".format(
-        dictionary=dictionary if dictionary else config.param('DEFAULT', 'genome_dictionary', type='filepath'),
-        beds=' '.join(beds)
+            dictionary=dictionary if dictionary else config.param('DEFAULT', 'genome_dictionary', type='filepath'),
+            beds=' '.join(beds)
         )
     )
 
@@ -215,8 +218,8 @@ python $PYTHON_TOOLS/preprocess.py \\
   --ref-depth RD --alt-depth AD \\
   {input} \\
   | bgzip -cf > {output}""".format(
-        input=input,
-        output=output
+            input=input,
+            output=output
         )
     )
 
@@ -231,9 +234,9 @@ def fix_varscan_output(input, output, options=None):
         command="""\
 python $PYTHON_TOOLS/fixVS2VCF.py {options} {input} \\
     {output}""".format(
-        options=options if options else "",
-        input=input if input else "",
-        output=output if input else "",
+            options=options if options else "",
+            input=input if input else "",
+            output=output if input else "",
         )
     )
 
@@ -270,9 +273,9 @@ bed2IntervalList.pl \\
   --dict {dictionary} \\
   --bed {bed} \\
   > {output}""".format(
-        dictionary=dictionary if dictionary else config.param('DEFAULT', 'genome_dictionary', type='filepath'),
-        bed=bed,
-        output=output
+            dictionary=dictionary if dictionary else config.param('DEFAULT', 'genome_dictionary', type='filepath'),
+            bed=bed,
+            output=output
         )
     )
 
@@ -305,10 +308,10 @@ rm {input_filename} """.format(
 {pre_gzip_command}filterLongIndel.pl \\
   {input} \\
   > {output}{post_rm_command}""".format(
-        pre_gzip_command=pre_gzip_command,
-        input=input_next,
-        output=output,
-        post_rm_command=post_rm_command
+            pre_gzip_command=pre_gzip_command,
+            input=input_next,
+            output=output,
+            post_rm_command=post_rm_command
         ),
         removable_files=[output]
     )
@@ -324,86 +327,90 @@ def vcf2bed(input, output):
         command="""\
 cat {input} | perl $PERL_TOOLS/vcf2bed.pl - \\
   > {output}""".format(
-        input=input,
-        output=output
+            input=input,
+            output=output
         )
     )
 
 def rnaseqLight_kallisto(fastq_file1, fastq_file2, transcriptome_file, tx2genes_file, output_dir, parameters, job_name):
     return Job(
-        input_files=[
-        fastq_file1,
-        fastq_file2,
-        transcriptome_file,
-        tx2genes_file],
-        output_files=[ os.path.join(output_dir, "abundance_transcripts.tsv"),
-                     os.path.join(output_dir, "abundance_genes.tsv") ],
-        module_entries=[
+        [
+            fastq_file1,
+            fastq_file2,
+            transcriptome_file,
+            tx2genes_file
+        ],
+        [
+            os.path.join(output_dir, "abundance_transcripts.tsv"),
+            os.path.join(output_dir, "abundance_genes.tsv")
+        ],
+        [
             ['DEFAULT', 'module_mugqic_tools'],
             ['DEFAULT', 'module_R'],
             ['kallisto', 'module_kallisto']
-            ],
+        ],
         name=job_name,
         command="""\
-            bash rnaseq_light_kallisto.sh \\
-            {fastq_file1} \\
-            {fastq_file2} \\
-            {transcriptome_file} \\
-            {tx2genes_file} \\
-            {output_dir} \\
-            {parameters}
-            """.format(
+bash rnaseq_light_kallisto.sh \\
+  {fastq_file1} \\
+  {fastq_file2} \\
+  {transcriptome_file} \\
+  {tx2genes_file} \\
+  {output_dir} \\
+  {parameters}""".format(
             fastq_file1=fastq_file1,
             fastq_file2=fastq_file2,
             transcriptome_file=transcriptome_file,
             tx2genes_file=tx2genes_file,
             output_dir=output_dir,
             parameters=parameters
-                )
+        )
      )
 
 ## functions for R tools ##
 def r_create_kallisto_count_matrix(input_abundance_files, output_dir, data_type, job_name):
     return Job(
-        input_files=input_abundance_files,
-        output_files=[os.path.join(output_dir, "all_readsets.abundance_" + data_type + ".csv")],
-        module_entries=[['DEFAULT', 'module_mugqic_tools'],
-                         ['DEFAULT', 'module_R'],
-                        ['DEFAULT', 'module_mugqic_R_packages']
-              ],
+        input_abundance_files,
+        [os.path.join(output_dir, "all_readsets.abundance_" + data_type + ".csv")],
+        [
+            ['DEFAULT', 'module_mugqic_tools'],
+            ['DEFAULT', 'module_R'],
+            ['DEFAULT', 'module_mugqic_R_packages']
+        ],
         name=job_name,
         command="""\
-            R --no-save --args \\
-            {input_abundance_files} \\
-            {output_dir} \\
-            {data_type} \\
-            < $R_TOOLS/mergeKallistoCounts.R""".format(
+R --no-save --args \\
+  {input_abundance_files} \\
+  {output_dir} \\
+  {data_type} \\
+  < $R_TOOLS/mergeKallistoCounts.R""".format(
             input_abundance_files=",".join(input_abundance_files),
             output_dir=output_dir,
             data_type=data_type #transcripts or genes
-            )
         )
+    )
 
 def r_create_kallisto_count_matrix(input_abundance_files, output_dir, data_type, job_name):
     return Job(
-        input_files=input_abundance_files,
-        output_files=[os.path.join(output_dir, "all_readsets.abundance_" + data_type + ".csv")],
-        module_entries=[['DEFAULT', 'module_mugqic_tools'],
-                         ['DEFAULT', 'module_R'],
-                        ['DEFAULT', 'module_mugqic_R_packages']
-              ],
+        input_abundance_files,
+        [os.path.join(output_dir, "all_readsets.abundance_" + data_type + ".csv")],
+        [
+            ['DEFAULT', 'module_mugqic_tools'],
+            ['DEFAULT', 'module_R'],
+            ['DEFAULT', 'module_mugqic_R_packages']
+        ],
         name=job_name,
         command="""\
-            R --no-save --args \\
-            {input_abundance_files} \\
-            {output_dir} \\
-            {data_type} \\
-            < $R_TOOLS/mergeKallistoCounts.R""".format(
+R --no-save --args \\
+  {input_abundance_files} \\
+  {output_dir} \\
+  {data_type} \\
+  < $R_TOOLS/mergeKallistoCounts.R""".format(
             input_abundance_files=",".join(input_abundance_files),
             output_dir=output_dir,
             data_type=data_type #transcripts or genes
-            )
         )
+    )
 
 def r_select_scaffolds(input, output, folder_sca, kmer, name_sample, type_insert, min_insert_size=200):
     return Job(
@@ -421,11 +428,11 @@ R --no-save --args \\
   {type_insert} \\
   {min_insert_size} \\
   < $R_TOOLS/puureAnalyseSelectSca.r""".format(
-        folder_sca=folder_sca,
-        kmer=kmer,
-        name_sample=name_sample,
-        type_insert=type_insert,
-        min_insert_size=min_insert_size
+            folder_sca=folder_sca,
+            kmer=kmer,
+            name_sample=name_sample,
+            type_insert=type_insert,
+            min_insert_size=min_insert_size
         )
     )
 
@@ -446,13 +453,13 @@ R --no-save --args \\
   {min_mapping_quality} \\
   {max_insert_size} \\
   < $R_TOOLS/puureAnalyseFindCluster{unmap_type}.r""".format(
-        folder_sca=folder_sca,
-        kmer=kmer,
-        name_sample=name_sample,
-        type_insert=type_insert,
-        min_mapping_quality=min_mapping_quality,
-        max_insert_size=max_insert_size,
-        unmap_type=unmap_type
+            folder_sca=folder_sca,
+            kmer=kmer,
+            name_sample=name_sample,
+            type_insert=type_insert,
+            min_mapping_quality=min_mapping_quality,
+            max_insert_size=max_insert_size,
+            unmap_type=unmap_type
         )
     )
 
@@ -475,14 +482,14 @@ R --no-save --args \\
   {min_overlap} \\
   {exclu_file} \\
   < $R_TOOLS/puureAnalyseFindInsert.r""".format(
-        folder_sca=folder_sca,
-        kmer=kmer,
-        name_sample=name_sample,
-        type_insert=type_insert,
-        mean_coverage=mean_coverage,
-        max_insert_size=max_insert_size,
-        min_overlap=min_overlap,
-        exclu_file=exclu_file
+            folder_sca=folder_sca,
+            kmer=kmer,
+            name_sample=name_sample,
+            type_insert=type_insert,
+            mean_coverage=mean_coverage,
+            max_insert_size=max_insert_size,
+            min_overlap=min_overlap,
+            exclu_file=exclu_file
         )
     )
 
@@ -506,15 +513,15 @@ R --no-save --args \\
   {min_num_read} \\
   {mean_read_length} \\
   < $R_TOOLS/puureAnalyseFindInsert.r""".format(
-        folder_sca=folder_sca,
-        kmer=kmer,
-        name_sample=name_sample,
-        type_insert=type_insert,
-        mean_coverage=mean_coverage,
-        max_insert_size=max_insert_size,
-        strand=strand,
-        min_num_read=min_num_read,
-        mean_read_length=mean_read_length
+            folder_sca=folder_sca,
+            kmer=kmer,
+            name_sample=name_sample,
+            type_insert=type_insert,
+            mean_coverage=mean_coverage,
+            max_insert_size=max_insert_size,
+            strand=strand,
+            min_num_read=min_num_read,
+            mean_read_length=mean_read_length
         )
     )
 
@@ -537,12 +544,12 @@ IHEC_rnaseq_metrics.sh \\
     {intergenic_bed} \\
     {rrna_bed} \\
     {output_dir}""".format(
-        input_bam=input_bam,
-        input_name=input_name,
-        input_picard_dup=input_picard_dup,
-        intergenic_bed=config.param('IHEC_rnaseq_metrics', 'intergenic_bed', type='filepath',required=True),
-        rrna_bed=config.param('IHEC_rnaseq_metrics', 'ribo_rna_bed', type='filepath',required=True),
-        output_dir=output_dir
+            input_bam=input_bam,
+            input_name=input_name,
+            input_picard_dup=input_picard_dup,
+            intergenic_bed=config.param('IHEC_rnaseq_metrics', 'intergenic_bed', type='filepath',required=True),
+            rrna_bed=config.param('IHEC_rnaseq_metrics', 'ribo_rna_bed', type='filepath',required=True),
+            output_dir=output_dir
         )
     )
 
@@ -565,22 +572,22 @@ def sh_ihec_chip_metrics(chip_bam, input_bam, sample_name, input_name, chip_type
         ],
         command="""\
 IHEC_chipseq_metrics.sh \\
-    -d {chip_bam} \\
-    -i {input_bam} \\
-    -s {sample_name} \\
-    -j {input_name} \\
-    -t {chip_type} \\
-    -n {threads} \\
-    -p {chip_bed} \\
-    -o {output_dir}""".format(
-        input_bam=input_bam,
-        input_name=input_name,
-        sample_name=sample_name,
-        chip_bam=chip_bam,
-        chip_type=chip_type,
-        threads=config.param('IHEC_chipseq_metrics', 'thread', type='int') if config.param('IHEC_chipseq_metrics', 'thread', type='int',required=False) else 1,
-        chip_bed=chip_bed,
-        output_dir=output_dir
+  -d {chip_bam} \\
+  -i {input_bam} \\
+  -s {sample_name} \\
+  -j {input_name} \\
+  -t {chip_type} \\
+  -n {threads} \\
+  -p {chip_bed} \\
+  -o {output_dir}""".format(
+            input_bam=input_bam,
+            input_name=input_name,
+            sample_name=sample_name,
+            chip_bam=chip_bam,
+            chip_type=chip_type,
+            threads=config.param('IHEC_chipseq_metrics', 'thread', type='int') if config.param('IHEC_chipseq_metrics', 'thread', type='int',required=False) else 1,
+            chip_bed=chip_bed,
+            output_dir=output_dir
         ),
         removable_files=[output_fingerprints,output_fingerprints_png,output_dedup_chip_bam,output_dedup_chip_bam,output_dedup_chip_bai,output_dedup_input_bam,output_dedup_input_bai,output_flagstats]
     )
