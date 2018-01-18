10c20693c0710181ac7f6cbdce89cd90c37bc728
Mathieu Bourgey <mathieu.bourgey@mcgill.ca>
2018-01-18 08:57:42 -0800

    remove lattency for jobsubimission to slurm

--- core/scheduler.py
+++ core/scheduler.py
@@ -320,8 +320,8 @@ exit \$MUGQIC_STATE" | \\
                     # Write job parameters in job list file
                     cmd += "\necho \"$" + job.id + "\t$JOB_NAME\t$JOB_DEPENDENCIES\t$JOB_OUTPUT_RELATIVE_PATH\" >> $JOB_LIST\n"
                     
-                    #add 2s sleep to let slurm submiting the job correctly
-                    cmd += "\nsleep 0.2\n"
+                    ##add 2s sleep to let slurm submiting the job correctly
+                    #cmd += "\nsleep 0.2\n"
 
                     print cmd
 
