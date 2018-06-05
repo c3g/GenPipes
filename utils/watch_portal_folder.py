e5e357955bbf0bdb77a532bde91a5c512d7a7cc6
Rom Grk <romgrk.cc@gmail.com>
2018-06-05 16:34:46 -0400

    watch_portal_folder: add logging

--- utils/watch_portal_folder.py
+++ utils/watch_portal_folder.py
@@ -40,7 +40,7 @@ def run(options):
     print('Found %i files' % len(files))
 
     details = []
-    for filename in files:
+    for i, filename in enumerate(files):
         filepath = os.path.join(options.watch_folder, filename)
         try:
             data = read_json(filepath)
@@ -48,16 +48,21 @@ def run(options):
             print(e)
             print(red('Failed to read file "%s". Skipping' % filename))
             continue
+        print('  Read %s (%i/%i)' % (filename, i + 1, len(files)))
         details.append({
             "filepath": filepath,
             "data": data
         })
+
+    print('Read %i files' % len(details))
 
     details_by_sample = group_by(details, lambda detail: detail['data']['sample_name'])
     for sample_name in details_by_sample:
         details = details_by_sample[sample_name]
         details.sort(key=lambda detail: os.path.getmtime(detail['filepath']))
         details.reverse()
+
+    print('Found %i samples' % len(details_by_sample.keys()))
 
     for sample_name in details_by_sample:
         send_files(options, sample_name, details_by_sample[sample_name])
@@ -113,7 +118,7 @@ def send_files(options, sample_name, details):
             return
 
         if response.status_code == 200 and result.get('ok') is True:
-            print('Sent %s' % filepath)
+            print('Sent %s (deleting %i files)' % (filepath, len(details)))
         else:
             print(red('Request failed %d ' % response.status_code) + ('[%s] %s: %s : %s' % (bold(url), file, response.reason, response.text)))
             return
@@ -261,6 +266,20 @@ def get_path_from_key(key):
 
 def get_value_from_key(root, key):
     return eval(key)
+
+
+# Benchmarking helpers
+
+def get_current_milliseconds():
+    return int(round(time.time() * 1000))
+
+labels = {}
+
+def start_timer(label):
+    labels[label] = get_current_milliseconds()
+
+def end_timer(label):
+    print('%s: %i' % (label, get_current_milliseconds() - labels[label]))
 
 
 if __name__ == '__main__':
