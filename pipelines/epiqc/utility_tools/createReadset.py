1cbf6d003853d116f943b82bcf0f2d84cebf3246
rami.coles@mail.mcgill.ca <rcoles@abacus3.ferrier.genome.mcgill.ca>
2019-08-23 15:46:22 -0400

    Added comments to the utility tools. ihec_json_parser.py creates a readset for epiqc and a list of marks to add in the base.ini file in the marks field of chromimpute section.

--- pipelines/epiqc/utility_tools/createReadset.py
+++ pipelines/epiqc/utility_tools/createReadset.py
@@ -1,5 +1,10 @@
 import os
 
+"""
+Searces for a directory named bigwig containing bigwig files in the working directory and creates
+an readset from those files.
+"""
+
 def make_readset():
     file = open("readset.tsv", "w+")
     file.write("Sample\tReadset\tLibrary\tRunType\tRun\tLane\tAdapter1\tAdapter2\tQualityOffset\tBED\tFASTQ1\tFASTQ2\tBAM\tUMI\tBIGWIG\n")
