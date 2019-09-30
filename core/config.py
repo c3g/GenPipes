ebb67ba72cc56e45cc6534d7adce2777f5009933
P-O Quirion <pioliqui@gmail.com>
2019-09-30 13:51:56 -0400

    Add default config for container ini

--- core/config.py
+++ core/config.py
@@ -79,7 +79,16 @@ class Config(ConfigParser.SafeConfigParser):
         cmd_query_module = "module {query_module} ".format(query_module  = query_module)
         for module in modules:
             # Bash shell must be invoked in order to find "module" cmd
-            module_show_output = subprocess.check_output(["bash", "-c", cmd_query_module + module], stderr=subprocess.STDOUT)
+            # try:
+            #     module_show_output = subprocess.check_output(["bash", "-c", cmd_query_module + module],
+            #                                                  stderr=subprocess.STDOUT)
+            # except subprocess.CalledProcessError:
+            #     module_show_output = subprocess.check_output(["singularity", "run", "/home/poq/c3g/genpipes.sif",
+            #                                                   cmd_query_module + module],
+            #                                                  stderr=subprocess.STDOUT)
+            module_show_output = subprocess.check_output(["bash", "-c", cmd_query_module + module],
+                                                         stderr=subprocess.STDOUT)
+
             ## "Error" result for module show while "error" for module spider. seems to be handeled well by re.IGNORECASE
             if re.search("Error", module_show_output, re.IGNORECASE):
                 raise Error("Error in config file(s) with " + module + ":\n" + module_show_output)
