79aff1763bd121baecf7ae8759083b1f1f25a4d4
pnawarathna <pubudu.nawarathna@mail.mcgill.ca>
2019-11-29 11:31:49 -0500

    hicseq completed adding basic features of the hicrep analysis.

--- bfx/hicrep.py
+++ bfx/hicrep.py
@@ -26,23 +26,48 @@ import os
 from core.config import *
 from core.job import *
 
-def calculate_reproducible_score( output_dir, sample1, sample2,chromosome ):
+def calculate_reproducible_score( output_dir, sample1, sample2, file1_path, file2_path,  chromosome ):
 
+    output_file= "".join((output_dir, "_".join(("/hicrep", sample1,  "vs", sample2 , chromosome)), ".tmp"))
     return Job(
-        [sample1,sample2],
-        [os.path.join("hicrep_score.tmp")],
+        [file1_path,file2_path],
+        [output_file],
         [
             ['reproducibility_scores', 'module_mugqic_tools'],
             ['reproducibility_scores', 'module_R']
         ],
         command="""\
+        mkdir -p {output_dir} &&
 Rscript /home/pubudu/projects/rrg-bourqueg-ad/pubudu/job_outputs/hicrep.R \\
-  -1 {sample1} \\
-  -2 {sample2} \\
+  -s1 {sample1} \\
+  -s2 {sample2} \\
+  -f1 {file1_path} \\
+  -f2 {file2_path} \\
   -c {chromosome} \\
   -o {output_dir}""".format(
         sample1=sample1,
         sample2=sample2,
+        file1_path=file1_path,
+        file2_path=file2_path,
         output_dir=output_dir,
         chromosome=chromosome
     ))
+
+
+def merge_files( input_files, temp_out_dir):
+    return Job(
+        input_files,
+        ["output_file"],
+        [
+            ['reproducibility_scores', 'module_mugqic_tools'],
+            ['reproducibility_scores', 'module_R']
+        ],
+        command="""\
+        cd {temp_out_dir} &&
+        for d in */ ; do \\
+        cat "$d"hicrep*.tmp | awk -v FS='\\t' -v OFS='\\t' -v dir="${{d%/*}}" '{{sum+=$1}} END {{print dir,sum/NR}}' > "${{d%/*}}".tmp \\
+        done &&
+        cat *.tmp > hicrep_reproducibilityscore_matrix.tsv &&
+        rm *.tmp""".format(
+        temp_out_dir=temp_out_dir
+        ))
