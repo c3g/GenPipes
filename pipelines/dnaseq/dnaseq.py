472c6b0e493a60f56edc5a63dfa52914590e8b89
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2020-03-03 11:55:09 -0500

    DNA-Seq - Fix update

--- pipelines/dnaseq/dnaseq.py
+++ pipelines/dnaseq/dnaseq.py
@@ -666,7 +666,7 @@ class DnaSeqRaw(common.Illumina):
         """
         Fix the read mates. Once local regions are realigned, the read mate coordinates of the aligned reads
         need to be recalculated since the reads are realigned at positions that differ from their original alignment.
-        Fixing the read mate positions is done using [BVATools](https://bitbucket.org/mugqic/bvatools).
+        Fixing the read mate positions is done using [Samtools](http://samtools.sourceforge.net/) and [Sambamba](http://lomereiter.github.io/sambamba/index.html).
         """
 
         jobs = []
@@ -686,7 +686,7 @@ class DnaSeqRaw(common.Illumina):
                 pipe_jobs([
                     samtools.fixmate(
                         input,
-                        "-",
+                        None,
                         config.param('fix_mate_by_coordinate_samtools', 'options')
                         ),
                     sambamba.sort(
@@ -2572,8 +2572,8 @@ cp {snv_metrics_prefix}.chromosomeChange.zip report/SNV.chromosomeChange.zip""".
                 self.sambamba_merge_sam_files,
                 self.gatk_indel_realigner,
                 self.sambamba_merge_realigned,
-                # self.fix_mate_by_coordinate,
-                self.fix_mate_by_coordinate_samtools,
+                self.fix_mate_by_coordinate,
+                # self.fix_mate_by_coordinate_samtools,
                 self.picard_mark_duplicates,
                 self.recalibration,
                 self.sym_link_final_bam,
@@ -2612,8 +2612,8 @@ cp {snv_metrics_prefix}.chromosomeChange.zip report/SNV.chromosomeChange.zip""".
                 self.sambamba_merge_sam_files,
                 self.gatk_indel_realigner,
                 self.sambamba_merge_realigned,
-                # self.fix_mate_by_coordinate,
-                self.fix_mate_by_coordinate_samtools,
+                self.fix_mate_by_coordinate,
+                # self.fix_mate_by_coordinate_samtools,
                 self.picard_mark_duplicates,
                 self.recalibration,
                 self.sym_link_final_bam,
