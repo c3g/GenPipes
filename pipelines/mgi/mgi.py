4b0740ea28449580ec958b821379c027765d4dc1
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2020-06-19 16:06:51 -0400

    Fixing path creation at host removal step

--- pipelines/mgi/mgi.py
+++ pipelines/mgi/mgi.py
@@ -173,7 +173,7 @@ class MGISeq(dnaseq.DnaSeqRaw):
 
             jobs.append(
                 concat_jobs([
-                    bash.mkdir(os.path.dirname(host_removal_directory)),
+                    bash.mkdir(os.path.dirname(readset_bam)),
                     pipe_jobs([
                         bwa.mem(
                             fastq1,
