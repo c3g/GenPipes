b5a4fb47e390a80cde8b07b76046168ed4211fe8
Edouard Henrion <edouard.henrion@mcgill.ca>
2020-09-03 10:20:34 -0400

    MGI Processing Pipeline - adding report step

--- bfx/bash_cmd.py
+++ bfx/bash_cmd.py
@@ -294,3 +294,13 @@ pigz {input} -c {output}""".format(
         )
     )
 
+def md5sum(input, output):
+    return Job(
+        [input],
+        [output],
+        command="""\
+md5sum {input} > {output}""".format(
+            input=os.path.abspath(input),
+            output=os.path.abspath(output)
+        )
+    )
