e3961cb8295f5e6eda367054d1d1a4d45265f6b3
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2021-01-20 17:35:40 -0500

    debug

--- pipelines/chipseq/chipseq.py
+++ pipelines/chipseq/chipseq.py
@@ -445,6 +445,7 @@ class ChipSeq(common.Illumina):
                             )
                     )
 
+        info.log("".join([os.path.join(self.output_dirs['alignment_output_directory'], sample.name, sample.name + "." + mark_name + ".sorted.filtered.bam") for mark_name in sample.mark_names] for sample in self.samples))
         report_file = os.path.join(self.output_dirs['report_output_directory'], "ChipSeq.samtools_view_filter.md")
         jobs.append(
             Job(
