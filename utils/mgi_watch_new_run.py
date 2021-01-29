58cca6b8c303e03d32bc4e09f1ded5912495c093
Edouard Henrion <edouard.henrion@mcgill.ca>
2021-01-29 13:46:41 -0500

    GenPipes - MGI Run Processing : corrected copy job arguments + rectified fastqc job name

--- utils/mgi_watch_new_run.py
+++ utils/mgi_watch_new_run.py
@@ -187,7 +187,6 @@ def compare_runs(
                             outdir,
                             process_dir,
                             genpipes_scr_dir,
-                            project,
                             run,
                             lane,
                             sequencer_path,
