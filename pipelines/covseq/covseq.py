edf5a762c25ee3245b5c5787e1e7d27f59e5567e
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2021-02-04 15:13:23 -0500

    Grouping rename jobs into 1 job

--- pipelines/covseq/covseq.py
+++ pipelines/covseq/covseq.py
@@ -879,6 +879,8 @@ class CoVSeQ(dnaseq.DnaSeqRaw):
         """
 
         jobs = []
+
+        job = bash.mkdir(os.path.join("consensus"))
         for sample in self.samples:
             consensus_directory = os.path.join("consensus", sample.name)
             [input_fa] = self.select_input_files([
@@ -909,13 +911,13 @@ class CoVSeQ(dnaseq.DnaSeqRaw):
 
             bedgraph_file = os.path.join(alignment_directory, re.sub("\.bam$", ".BedGraph", os.path.basename(input_bam)))
 
-            jobs.append(
-                concat_jobs([
-                    bash.mkdir(os.path.dirname(output_fa)),
-                    Job(
-                        input_files=[quast_tsv, quast_html],
-                        output_files=[],
-                        command="""\\
+            job = concat_jobs([
+                job,
+                bash.mkdir(os.path.dirname(output_fa)),
+                Job(
+                    input_files=[quast_tsv, quast_html],
+                    output_files=[],
+                    command="""\\
 cons_len=`grep -oP "Total length \(>= 0 bp\)\\t\K.*?(?=$)" {quast_tsv}`
 N_count=`grep -oP "# N's\\",\\"quality\\":\\"Less is better\\",\\"values\\":\[\K.*?(?=])" {quast_html}`
 cons_perc_N=`echo "scale=2; 100*$N_count/$cons_len" | bc -l`
@@ -931,11 +933,11 @@ export STATUS""".format(
     annotated_vcf=annotated_vcf,
     bedgraph_file=bedgraph_file
     )
-                        ),
-                    Job(
-                        input_files=[input_fa],
-                        output_files=[output_fa],
-                        command="""\\
+                    ),
+                Job(
+                    input_files=[input_fa],
+                    output_files=[output_fa],
+                    command="""\\
 awk '/^>/{{print ">{country}/{province}-{sample}/{year} seq_method:{seq_method}|assemb_method:{assemb_method}|snv_call_method:{snv_call_method}"; next}}{{print}}' < {input_fa} > {output_status_fa} && \\
 ln -sf {output_status_fa_basename} {output_fa}
 """.format(
@@ -951,12 +953,62 @@ ln -sf {output_status_fa_basename} {output_fa}
     output_status_fa=output_status_fa,
     output_fa=output_fa
     )
-                        )
-                ],
-                name="rename_consensus_header." + sample.name,
-                samples=[sample]
-                )
+                    )
+            ],
+            name="rename_consensus_header." + sample.name,
+            samples=[sample]
             )
+
+        jobs.append(job)
+
+#             jobs.append(
+#                 concat_jobs([
+#                     bash.mkdir(os.path.dirname(output_fa)),
+#                     Job(
+#                         input_files=[quast_tsv, quast_html],
+#                         output_files=[],
+#                         command="""\\
+# cons_len=`grep -oP "Total length \(>= 0 bp\)\\t\K.*?(?=$)" {quast_tsv}`
+# N_count=`grep -oP "# N's\\",\\"quality\\":\\"Less is better\\",\\"values\\":\[\K.*?(?=])" {quast_html}`
+# cons_perc_N=`echo "scale=2; 100*$N_count/$cons_len" | bc -l`
+# frameshift=`if grep -q "frameshift_variant" {annotated_vcf}; then echo "FLAG"; fi`
+# genome_size=`awk '{{print $2}}' {genome_file}`
+# bam_cov50X=`awk '{{if ($4 > 50) {{count = count + $3-$2}}}} END {{if (count) {{print count}} else {{print 0}}}}' {bedgraph_file}`
+# bam_cov50X=`echo "scale=2; 100*$bam_cov50X/$genome_size" | bc -l`
+# STATUS=`awk -v bam_cov50X=$bam_cov50X -v frameshift=$frameshift -v cons_perc_N=$cons_perc_N 'BEGIN {{ if (cons_perc_N < 1 && frameshift != "FLAG" && bam_cov50X >= 90) {{print "pass"}}  else if (cons_perc_N > 5) {{print "rej"}} else if ((cons_perc_N >= 1 && cons_perc_N <= 5) || frameshift == "FLAG" || bam_cov50X < 90) {{print "flag"}} }}'`
+# export STATUS""".format(
+#     quast_html=quast_html,
+#     quast_tsv=quast_tsv,
+#     genome_file=config.param('DEFAULT', 'igv_genome', required=False),
+#     annotated_vcf=annotated_vcf,
+#     bedgraph_file=bedgraph_file
+#     )
+#                         ),
+#                     Job(
+#                         input_files=[input_fa],
+#                         output_files=[output_fa],
+#                         command="""\\
+# awk '/^>/{{print ">{country}/{province}-{sample}/{year} seq_method:{seq_method}|assemb_method:{assemb_method}|snv_call_method:{snv_call_method}"; next}}{{print}}' < {input_fa} > {output_status_fa} && \\
+# ln -sf {output_status_fa_basename} {output_fa}
+# """.format(
+#     country=config.param('rename_consensus_header', 'country', required=False),
+#     province=config.param('rename_consensus_header', 'province', required=False),
+#     year=config.param('rename_consensus_header', 'year', required=False),
+#     seq_method=config.param('rename_consensus_header', 'seq_method', required=False),
+#     assemb_method=config.param('rename_consensus_header', 'assemb_method', required=False),
+#     snv_call_method=config.param('rename_consensus_header', 'snv_call_method', required=False),
+#     sample=sample.name,
+#     input_fa=input_fa,
+#     output_status_fa_basename=os.path.basename(output_status_fa),
+#     output_status_fa=output_status_fa,
+#     output_fa=output_fa
+#     )
+#                         )
+#                 ],
+#                 name="rename_consensus_header." + sample.name,
+#                 samples=[sample]
+#                 )
+#             )
 
         return jobs
 
