706ccb32be13e72861d385ba8e8875b8bbe8288f
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2021-04-13 16:58:58 -0400

    Debug

--- core/pipeline.py
+++ core/pipeline.py
@@ -367,7 +367,7 @@ class Pipeline(object):
 
                 log.debug("Job name: " + job.name)
                 log.debug("Job input files:\n  " + "\n  ".join(job.input_files))
-                log.debug("Job output files:\n  " + "\n  ".join(job.output_files))
+                log.debug("Job output files:\n  " + "\n  ".join(job.output_files) + "\n")
 
                 # Job .done file name contains the command checksum.
                 # Thus, if the command is modified, the job is not up-to-date anymore.
