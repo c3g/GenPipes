96ee305658bc7aa5d6d3fee9697873fe85e3c8a8
P-O Quirion <pierre-olivier.quirion@computationalgenomics.ca>
2021-05-26 16:08:18 -0400

    used image v2.0.0.rc in for container

--- resources/container/get_wrapper.sh
+++ resources/container/get_wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Get Genpipes In A Container image
+GIAC_VERSION=v2.0.0.rc
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-wget -c http://github.com/c3g/genpipes_in_a_container/releases/download/1.1.0/wrapper_genpipes.tgz -O - | tar -xz -C ${SCRIPT_DIR}   --strip-components=1
+wget -c http://github.com/c3g/genpipes_in_a_container/releases/download/${GIAC_VERSION}/wrapper_genpipes.tgz -O - | tar -xz -C ${SCRIPT_DIR}   --strip-components=1
 echo Installing singulaity image and configuration file
+echo If ${SCRIPT_DIR}/etc/wrapper.conf is already configured, you should probably not overwirte it
 mv -i ${SCRIPT_DIR}/etc/wrapper.conf.tpl ${SCRIPT_DIR}/etc/wrapper.conf
 
