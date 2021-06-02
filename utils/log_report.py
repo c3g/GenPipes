944fcc7d0516d9c8582e0313f5c26a5b9dc0130d
P-O Quirion <pierre-olivier.quirion@computationalgenomics.ca>
2021-06-02 14:04:08 -0400

    force log report to python 2 :

--- utils/log_report.py
+++ utils/log_report.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 ################################################################################
 # Copyright (C) 2014, 2015 GenAP, McGill University and Genome Quebec Innovation Centre
