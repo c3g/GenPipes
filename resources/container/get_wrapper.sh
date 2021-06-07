6d6b28bc69789bfbcdea8798bdbb1454ef1f8ff8
P-O Quirion <pierre-olivier.quirion@computationalgenomics.ca>
2021-06-07 16:39:46 -0400

    update get_wrapper to v2.0.1

--- resources/container/get_wrapper.sh
+++ resources/container/get_wrapper.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 # Get Genpipes In A Container image
-GIAC_VERSION=v2.0.0.rc
+GIAC_VERSION=v2.0.1
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 wget -c http://github.com/c3g/genpipes_in_a_container/releases/download/${GIAC_VERSION}/wrapper_genpipes.tgz -O - | tar -xz -C ${SCRIPT_DIR}   --strip-components=1
 echo Installing singulaity image and configuration file
-echo If ${SCRIPT_DIR}/etc/wrapper.conf is already configured, you should probably not overwirte it
-mv -i ${SCRIPT_DIR}/etc/wrapper.conf.tpl ${SCRIPT_DIR}/etc/wrapper.conf
+if test -f "${SCRIPT_DIR}/etc/wrapper.conf"; then
+  rel=$(realpath --relative-to=$PWD ${SCRIPT_DIR}/etc/wrapper.conf)
+  echo "./$rel exist,
+  read -r -p  "  do you want to overwrite it y[n]:?" response
+
+   case "$response" in
+     [yY][eE][sS]|[yY])
+          echo updating confing
+          mv  ${SCRIPT_DIR}/etc/wrapper.conf.tpl ${SCRIPT_DIR}/etc/wrapper.conf
+          ;;
+     *)
+          echo keeping old config
+          ;;
+    esac
+else
+  echo mv -i ${SCRIPT_DIR}/etc/wrapper.conf.tpl ${SCRIPT_DIR}/etc/wrapper.conf
+fi
+
 
