e263de6e4b9cb6e96f8ffee3716408806e5064b7
Pubudu Nawarathna Mudiyanselage <pubudu@narval1.narval.calcul.quebec>
2022-03-31 13:50:47 -0400

    Added variant calling using GATK4 to chipseq pipeline

--- pipelines/chipseq/chipseq.py
+++ pipelines/chipseq/chipseq.py
@@ -2354,6 +2354,7 @@ done""".format(
                     )
                     )
 
+
         return jobs
 
     def merge_and_call_individual_gvcf(self):
@@ -2395,14 +2396,13 @@ done""".format(
                     )
         return jobs
 
-
     @property
     def steps(self):
         return [
             [
                 self.picard_sam_to_fastq,
                 self.trimmomatic,
-                self.merge_trimmomatic_stats,
+               self.merge_trimmomatic_stats,
                 self.mapping_bwa_mem_sambamba,
                 self.sambamba_merge_bam_files,
                 self.sambamba_mark_duplicates,
