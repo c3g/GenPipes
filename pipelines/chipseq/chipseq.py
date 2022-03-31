d18e0107a0466f9e53ecec82f9cb4d4c98e6ac3f
Pubudu Nawarathna Mudiyanselage <pubudu@narval1.narval.calcul.quebec>
2022-03-31 13:53:48 -0400

    fixed a bug in input file list of haplotypecaller extended the function to change inter-padding

--- pipelines/chipseq/chipseq.py
+++ pipelines/chipseq/chipseq.py
@@ -2263,9 +2263,6 @@ done""".format(
                     )
                     )
 
-
-        return jobs
-
     def merge_and_call_individual_gvcf(self):
         """
         Merges the gvcfs of haplotype caller and also generates a per sample vcf containing genotypes.
@@ -2311,7 +2308,7 @@ done""".format(
             [
                 self.picard_sam_to_fastq,
                 self.trimmomatic,
-               self.merge_trimmomatic_stats,
+                self.merge_trimmomatic_stats,
                 self.mapping_bwa_mem_sambamba,
                 self.sambamba_merge_bam_files,
                 self.sambamba_mark_duplicates,
