d83071de7d055f158f7a4b86cd68e34f5c1c5b33
ehenrion <edouard.henrion@mcgill.ca>
2023-03-08 21:55:43 +0000

    Resources - Adding install script for hmmcopy-utils and ichorCNA

--- resources/modules/hmmcopy-utils.sh
+++ resources/modules/hmmcopy-utils.sh
@@ -2,20 +2,24 @@
 # Exit immediately on error
 set -eu -o pipefail
 
-SOFTWARE=fastqc
-VERSION=0.12.1
-ARCHIVE=$SOFTWARE-$VERSION.zip
-ARCHIVE_URL=http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v${VERSION}.zip
-SOFTWARE_DIR=$SOFTWARE-$VERSION
+SOFTWARE=hmmcopy_utils
+COMMIT=29a8d1d
+VERSION=master-${COMMIT}
+ARCHIVE=${SOFTWARE}-${VERSION}.zip
+ARCHIVE_URL=https://github.com/shahcompbio/${SOFTWARE}/archive/refs/heads/master.zip
+SOFTWARE_DIR=${SOFTWARE}-${VERSION}
 
 build() {
-  cd $INSTALL_DOWNLOAD
-  unzip $ARCHIVE
-  mv FastQC $SOFTWARE_DIR
-  chmod +x $SOFTWARE_DIR/fastqc
-  chmod -R g+w $SOFTWARE_DIR
-  sed -i s,"#\!/usr/bin/perl.*,#\!/usr/bin/env perl,g" $SOFTWARE_DIR/fastqc
-  mv -i $SOFTWARE_DIR $INSTALL_DIR/
+  cd ${INSTALL_DOWNLOAD}
+
+  git clone https://github.com/shahcompbio/${SOFTWARE}.git
+  cd ${SOFTWARE}
+  git checkout ${COMMIT}
+  cmake .
+  make
+
+  cd ${INSTALL_DOWNLOAD}
+  mv -i ${SOFTWARE} ${INSTALL_DIR}/${SOFTWARE_DIR}
 }
 
 module_file() {
@@ -27,11 +31,10 @@ proc ModulesHelp { } {
 module-whatis \"$SOFTWARE\"
                       
 set             root                $INSTALL_DIR/$SOFTWARE_DIR
-prepend-path    PATH                \$root
+prepend-path    PATH                \$root/bin
 "
 }
 
 # Call generic module install script once all variables and functions have been set
 MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $MODULE_INSTALL_SCRIPT_DIR/install_module.sh $@
-
---
+++ resources/modules/ichorcna.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Exit immediately on error
+set -eu -o pipefail
+
+SOFTWARE=ichorCNA
+VERSION=0.2.0
+ARCHIVE=${SOFTWARE}-${VERSION}.tar.gz
+ARCHIVE_URL=https://github.com/broadinstitute/${SOFTWARE}/archive/refs/tags/v${VERSION}.tar.gz
+SOFTWARE_DIR=${SOFTWARE}-${VERSION}
+R_MODULE=mugqic/R_Bioconductor/4.2.2_3.16
+
+build() {
+  cd ${INSTALL_DOWNLOAD}
+
+  rm -rf ${SOFTWARE}
+  git clone https://github.com/broadinstitute/${SOFTWARE}.git -b v${VERSION}
+
+  module load ${R_MODULE}
+  mkdir -p ${INSTALL_DIR}/${SOFTWARE_DIR}
+
+  cat << EOF > install_r_packages.R
+install.packages("plyr", repos='http://cran.rstudio.org', lib="${INSTALL_DIR}/${SOFTWARE_DIR}")
+BiocManager::install("HMMcopy", lib="${INSTALL_DIR}/${SOFTWARE_DIR}")
+BiocManager::install("GenomeInfoDb", lib="${INSTALL_DIR}/${SOFTWARE_DIR}")
+BiocManager::install("GenomicRanges", lib="${INSTALL_DIR}/${SOFTWARE_DIR}")
+EOF
+
+  Rscript --vanilla install_r_packages.R
+  R CMD INSTALL -l ${INSTALL_DIR}/${SOFTWARE_DIR} ${SOFTWARE}
+  cp -r ${SOFTWARE}/scripts ${INSTALL_DIR}/${SOFTWARE_DIR}/scripts
+}
+
+module_file() {
+echo "\
+#%Module1.0
+proc ModulesHelp { } {
+       puts stderr \"\tMUGQIC - $SOFTWARE \"
+}
+module-whatis \"$SOFTWARE\"
+                      
+set             root                $INSTALL_DIR/$SOFTWARE_DIR
+prepend-path    R_LIBS              \$root
+setenv          ICHORCNA_HOME       \$root/scripts
+"
+}
+
+# Call generic module install script once all variables and functions have been set
+MODULE_INSTALL_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source $MODULE_INSTALL_SCRIPT_DIR/install_module.sh $@
