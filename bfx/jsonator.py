a458c39085dccabbcc410d2baa495f1b935dda4c
ehenrion <edouard.henrion@mcgill.ca>
2023-03-23 17:37:33 +0000

    GenPipes - strengthening the jsonator

--- bfx/jsonator.py
+++ bfx/jsonator.py
@@ -17,6 +17,8 @@
 # along with MUGQIC Pipelines.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
+# Python Standard Modules
+import logging
 import os
 import re
 import json
@@ -24,9 +26,11 @@ import json
 # MUGQIC Modules
 from core.config import config
 
+log = logging.getLogger(__name__)
+
 # Start creating the json dump for the passed sample
 def create(pipeline, sample):
-    jsonator_version = "1.0.1"
+    jsonator_version = "1.0.2"
 
     # Retrieve the project name fome the config file, if not specified then use the parent folder name of where the pipeline has been launched
     if config.param("DEFAULT", 'project_name', required=False):
@@ -98,13 +102,81 @@ def create(pipeline, sample):
     # Or    if the json file has not been created yet :
     if pipeline.force_jobs or not os.path.exists(os.path.join(pipeline.output_dir, "json", sample.json_file)) or os.stat(os.path.join(pipeline.output_dir, "json", sample.json_file)).st_size == 0:
         # Then (re-)create it !!
-        if pipeline.__class__.__name__ == "PacBioAssembly":
-            json_hash = {
-                'version': jsonator_version,
-                'project': project_name,
-                'submission_date': "",      # Create a submission time entry and let it empty : will be updated as the bash script is launched
-                'sample_name' : sample.name,
-                'readset' : [{
+        log.debug(f"Creating analysis JSON for sample {sample.name}")
+        json_hash = create_json(
+            sample,
+            pipeline,
+            project_name,
+            general_info,
+            softwares,
+            jsonator_version
+        )
+
+    # If the json file has already been created (during a previous pipeline execution for instance) :
+    else :
+        log.debug(f"Opening for update existing analysis JSON for sample '{sample.name}'.")
+        try:
+            with open(os.path.join(pipeline.output_dir, "json", sample.json_file), 'r') as json_file:
+                current_json_hash = json.load(json_file)
+            if current_json_hash:
+                json_hash = update_json(
+                    current_json_hash,
+                    sample,
+                    pipeline,
+                    general_info,
+                    softwares
+                )
+            else:
+                log.debug(f"Existing analysis JSON for sample '{sample.name}' is empty... re-creating it.")
+                json_hash = create_json(
+                    sample,
+                    pipeline,
+                    project_name,
+                    general_info,
+                    softwares,
+                    jsonator_version
+                )
+
+        except json.decoder.JSONDecodeError:
+            log.debug(f"Update failed... re-creating analysis JSON for sample '{sample.name}'.")
+            json_hash = create_json(
+                sample,
+                pipeline,
+                project_name,
+                general_info,
+                softwares,
+                jsonator_version
+            )
+    
+    current_json = json.dumps(json_hash, indent=4, default=str)
+
+    if not os.path.exists(os.path.join(pipeline.output_dir, "json")):
+        os.makedirs(os.path.join(pipeline.output_dir, "json"))
+
+    # Print to file
+    filepath = os.path.join(pipeline.output_dir, "json", sample.json_file)
+    with open(filepath, 'w') as out_json:
+        out_json.write(current_json)
+
+    return filepath
+
+
+def create_json(
+    sample,
+    pipeline,
+    project,
+    general_info,
+    softwares,
+    jsonator_version
+    ):
+    if pipeline.__class__.__name__ == "PacBioAssembly":
+        json_hash = {
+            'version': jsonator_version,
+            'project': project,
+            'submission_date': "",      # Create a submission time entry and let it empty : will be updated as the bash script is launched
+            'sample_name' : sample.name,
+            'readset' : [
+                {
                     "name" : readset.name,
                     "run" : readset.run,
                     "smartcell" : readset.smartcell,
@@ -113,48 +185,56 @@ def create(pipeline, sample):
                     "estimated_genome_size" : readset.estimated_genome_size,
                     "bas" : [os.path.realpath(bas) for bas in readset.bas_files],
                     "bax" : [os.path.realpath(bax) for bax in readset.bax_files]
-                } for readset in sample.readsets],
-                'pipeline' : {
-                    'name' : pipeline.__class__.__name__,
-                    'general_information': general_info,
-                    'software' : [{
+                } for readset in sample.readsets
+            ],
+            'pipeline' : {
+                'name' : pipeline.__class__.__name__,
+                'general_information': general_info,
+                'software' : [
+                    {
                         'name' : software['name'],
                         'version' : software['version']
-                    } for software in softwares],
-                    'step': []
-                }
+                    } for software in softwares
+                ],
+                'step': []
             }
-        elif pipeline.__class__.__name__ == "Nanopore" or pipeline.__class__.__name__ == "NanoporeCoVSeq":
-            json_hash = {
-                'version': jsonator_version,
-                'project': project_name,
-                'submission_date': "",
-                # Create a submission time entry and let it empty : will be updated as the bash script is launched
-                'sample_name': sample.name,
-                'readset': [{
+        }
+    elif pipeline.__class__.__name__ == "Nanopore" or pipeline.__class__.__name__ == "NanoporeCoVSeq":
+        json_hash = {
+            'version': jsonator_version,
+            'project': project,
+            'submission_date': "",
+            # Create a submission time entry and let it empty : will be updated as the bash script is launched
+            'sample_name': sample.name,
+            'readset': [
+                {
                     "name": readset.name,
                     "run": readset.run,
                     "flowcell": readset.flowcell,
                     "library": readset.library,
                     "summary": readset.summary_file,
-                } for readset in sample.readsets],
-                'pipeline': {
-                    'name': pipeline.__class__.__name__,
-                    'general_information': general_info,
-                    'software': [{
+                } for readset in sample.readsets
+            ],
+            'pipeline': {
+                'name': pipeline.__class__.__name__,
+                'general_information': general_info,
+                'software': [
+                    {
                         'name': software['name'],
                         'version': software['version']
-                    } for software in softwares],
-                    'step': []
-                }
+                    } for software in softwares
+                ],
+                'step': []
             }
-        else :
-            json_hash = {
-                'version': jsonator_version,
-                'project': project_name,
-                'submission_date': "",      # Create a submission time entry and let it empty : will be updated as the bash script is launched
-                'sample_name' : sample.name,
-                'readset' : [{
+        }
+    else :
+        json_hash = {
+            'version': jsonator_version,
+            'project': project,
+            'submission_date': "",      # Create a submission time entry and let it empty : will be updated as the bash script is launched
+            'sample_name' : sample.name,
+            'readset' : [
+                {
                     "name" : readset.name,
                     "library" : readset.library,
                     "runType" : readset.run_type,
@@ -167,110 +247,109 @@ def create(pipeline, sample):
                     "fastq1" : os.path.realpath(readset.fastq1) if readset.fastq1 else "",
                     "fastq2" : os.path.realpath(readset.fastq2) if readset.fastq2 else "",
                     "bam" : os.path.realpath(readset.bam) if readset.bam else ""
-                } for readset in sample.readsets],
-                'pipeline' : {
-                    'name' : pipeline.__class__.__name__,
-                    'general_information': general_info,
-                    'software' : [{
+                } for readset in sample.readsets
+            ],
+            'pipeline' : {
+                'name' : pipeline.__class__.__name__,
+                'general_information': general_info,
+                'software' : [
+                    {
                         'name' : software['name'],
                         'version' : software['version']
-                    } for software in softwares],
-                    'step': []
-                }
+                    } for software in softwares
+                ],
+                'step': []
             }
-        for step in pipeline.step_range:
-            # First verify if the step is meant to be "jsonified"
-            jsonify_step = False
+        }
+    for step in pipeline.step_range:
+        # First verify if the step is meant to be "jsonified"
+        jsonify_step = False
+        for job in step.jobs:
+            if sample in job.samples:
+                jsonify_step = True
+        if jsonify_step:
+            json_hash['pipeline']['step'].append(
+                {
+                    'name': step.name,
+                    'job': [
+                        {
+                            "name": job.name,
+                            "id": job.id,
+                            "command": re.sub("\\\\\n", "", job.command_with_modules),
+                            "input_file": job.input_files,
+                            "output_file": job.output_files,
+                            "dependency": [dependency_job.id for dependency_job in job.dependency_jobs]
+                        } for job in step.jobs if sample in job.samples
+                    ]
+                }
+            )
+    return json_hash
+
+def update_json(
+        current_json_hash,
+        sample,
+        pipeline,
+        general_info,
+        softwares
+    ):
+
+    # Then check if information is up-to-date by comparing it with the previously retrieved informations
+    for info_key in general_info.keys():
+        if not info_key in current_json_hash['pipeline']['general_information'] or current_json_hash['pipeline']['general_information'][info_key] != general_info[info_key] :
+            current_json_hash['pipeline']['general_information'][info_key] = general_info[info_key]
+
+    # And do the same checking with the list of softwares
+    for soft in softwares:
+        soft_found = False
+        for jsoft in current_json_hash['pipeline']['software']:
+            if soft['name'] == jsoft['name']:
+                soft_found = True
+                if soft['version'] != jsoft['version']:
+                    jsoft['version'] = soft['version']
+        if not soft_found:
+            current_json_hash['pipeline']['software'].append(
+                {
+                    'name' : soft['name'],
+                    'version' : soft['version']
+                }
+            )
+
+    # Finally check if the requested steps/jobs are already in the JSON :
+    #   if so  : update them with the current information
+    #   if not : add them to the json object
+    for step in pipeline.step_range:
+        # First make sure the step is meant to be "jsonified"
+        jsonify_step = False
+        if step.jobs:
             for job in step.jobs:
                 if sample in job.samples:
                     jsonify_step = True
-            if jsonify_step:
-                json_hash['pipeline']['step'].append(
-                    {
-                        'name': step.name,
-                        'job': [{
-                            "name": job.name,
-                            "id": job.id,
-                            "command": re.sub("\\\\\n", "", job.command_with_modules),
-                            "input_file": job.input_files,
-                            "output_file": job.output_files,
-                            "dependency": [dependency_job.id for dependency_job in job.dependency_jobs]
-                        } for job in step.jobs if sample in job.samples]
-                    }
-                )
-        current_json = json.dumps(json_hash, indent=4, default=str)
 
-    # If the json file has already been created (during a previous pipeline execution for instance) :
-    else :
-        with open(os.path.join(pipeline.output_dir, "json", sample.json_file), 'r') as json_file:
-            current_json_hash = json.load(json_file)
+        if jsonify_step:
+            # Then check if the step is found in the current json
+            step_found = False
+            for jstep in current_json_hash['pipeline']['step']:
+                if step.name == jstep['name']:
+                    step_found = True
 
-        # Then check if information is up-to-date by comparing it with the previously retrieved informations
-        for info_key in general_info.keys():
-            if not info_key in current_json_hash['pipeline']['general_information'] or current_json_hash['pipeline']['general_information'][info_key] != general_info[info_key] :
-                current_json_hash['pipeline']['general_information'][info_key] = general_info[info_key]
+            # If step is found, then remove it from the json object (so that it can be replaced by the new one if needed)
+            if step_found:
+                for i in range(len(current_json_hash['pipeline']['step'])):
+                    if current_json_hash['pipeline']['step'][i]['name'] == step.name:
+                        del current_json_hash['pipeline']['step'][i]
+                        break
 
-        # And do the same checking with the list of softwares
-        for soft in softwares:
-            soft_found = False
-            for jsoft in current_json_hash['pipeline']['software']:
-                if soft['name'] == jsoft['name']:
-                    soft_found = True
-                    if soft['version'] != jsoft['version']:
-                        jsoft['version'] = soft['version']
-            if not soft_found:
-                current_json_hash['pipeline']['software'].append({
-                    'name' : soft['name'],
-                    'version' : soft['version']
-                })
-
-        # Finally check if the requested steps/jobs are already in the JSON :
-        #   if so  : update them with the current information
-        #   if not : add them to the json object
-        for step in pipeline.step_range:
-            # First make sure the step is meant to be "jsonified"
-            jsonify_step = False
-            if step.jobs:
-                for job in step.jobs:
-                    if sample in job.samples:
-                        jsonify_step = True
-
-            if jsonify_step:
-                # Then check if the step is found in the current json
-                step_found = False
-                for jstep in current_json_hash['pipeline']['step']:
-                    if step.name == jstep['name']:
-                        step_found = True
-
-                # If step is found, then remove it from the json object (so that it can be replaced by the new one if needed)
-                if step_found:
-                    for i in range(len(current_json_hash['pipeline']['step'])):
-                        if current_json_hash['pipeline']['step'][i]['name'] == step.name:
-                            del current_json_hash['pipeline']['step'][i]
-                            break
-
-                # Now it is time to add the current step record (with its jobs) to the json object
-                current_json_hash['pipeline']['step'].append(
-                    {
-                        'name': step.name,
-                        'job': [{
-                            "name": job.name,
-                            "id": job.id,
-                            "command": re.sub("\\\\\n", "", job.command_with_modules),
-                            "input_file": job.input_files,
-                            "output_file": job.output_files,
-                            "dependency": [dependency_job.id for dependency_job in job.dependency_jobs]
-                        } for job in step.jobs if sample in job.samples]
-                    }
-                )
-        current_json = json.dumps(current_json_hash, indent=4, default=str)
-
-    if not os.path.exists(os.path.join(pipeline.output_dir, "json")):
-        os.makedirs(os.path.join(pipeline.output_dir, "json"))
-
-    # Print to file
-    filepath = os.path.join(pipeline.output_dir, "json", sample.json_file)
-    with open(filepath, 'w') as out_json:
-        out_json.write(current_json)
-
-    return filepath
+            # Now it is time to add the current step record (with its jobs) to the json object
+            current_json_hash['pipeline']['step'].append(
+                {
+                    'name': step.name,
+                    'job': [{
+                        "name": job.name,
+                        "id": job.id,
+                        "command": re.sub("\\\\\n", "", job.command_with_modules),
+                        "input_file": job.input_files,
+                        "output_file": job.output_files,
+                        "dependency": [dependency_job.id for dependency_job in job.dependency_jobs]
+                    } for job in step.jobs if sample in job.samples]
+                }
+            )
