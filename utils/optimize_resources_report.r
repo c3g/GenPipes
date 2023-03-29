198de15a701c3c5cf8994080a277667c0e1bb46c
Matteo Leguen <matteo.leguen@mail.mcgill.ca>
2023-03-29 12:11:25 -0400

    Research function added, .o research among job_output folders complete. Both running.

--- utils/optimize_resources_report.r
+++ utils/optimize_resources_report.r
@@ -12,32 +12,106 @@
 #job_output_path <- readLines("stdin",n=1);
 
 #à modifier, permet de pas avoir à rentrer le nom du fichier à chaque fois
-job_output_path = "job_output"
+job_output_path = "~/Documents/local/projet/optimize_resources_report/job_output"
 
 
 #list all folders path in job_output folder as list
-job_output_list_folder <- as.list(list.dirs(path = job_output_path,
+job_output_list_folder <- (list.dirs(path = job_output_path,
 									recursive = FALSE,
 									full.names = TRUE))
 
-#Same parsing and values compilation for each folder
+#list containing sub list for each folder
 o_files_list = list() 
 
-
-#Create list of .o file
-	# sub list for each folder
-
-for (folder_path in job_output_list_folder) {
-	# open folder and parse .o files 
-
-	o_files_list <- append(o_files_list, list(list.files(folder_path,
-								pattern = "\\.o$",
-								recursive = TRUE,
-								full.names = TRUE)))
+for (i in 1:length(job_output_list_folder)) {
+	# open each folder from folder list 
+  folder_path <- job_output_list_folder[i]
+  
+  # find all .o file in each sub folder
+  new_folder <- list(list.files(folder_path,
+                           pattern = "\\.o$",
+                           recursive = TRUE,
+                           full.names = TRUE))
+  
+  # some folder doesn't have any .o file, condition to avoid type error
+  if (length(new_folder) == 0 ){
+    o_files_list[i] <- NULL
+  }else{
+    o_files_list[i] <- new_folder
+  }
 }
+
 
 parsed_folder <- function(file_path_list){
 	#IN : list of .o file path
 	#OUT : dataset containing wanted information from these files
-	
+	for (i in file_path_list){
+		for (j in i){
+		  #file reading
+		  FileInput = readLines(j)
+		  
+		  # Replace line feeds with spaces
+		  FileInput_Space <- gsub(pattern = "\\n", replacement = " ", x = FileInput)
+
+		  #cut before FAKE EPILOGUE
+		  #EndFileInput_Space <- strsplit(x = FileInput_Space, split = "SLURM FAKE EPILOGUE (MUGQIC)")
+		  
+		  #split file to extract value
+		  FileInput_List <- strsplit(x = FileInput_Space, split = " ")#[[1]]
+		  
+
+		  #research EligibleTime
+		  EligibleTime <- research_Element(FileInput_List, "EligibleTime")
+		  
+		  #research StartTime
+		  StartTime <- research_Element(FileInput_List, "StartTime")
+
+		  #research RunTime
+		  RunTime <- research_Element(FileInput_List, "RunTime")
+
+		  #research TimeLimit
+		  TimeLimit <- research_Element(FileInput_List, "TimeLimit")
+
+		  #research NumCPUs
+		  NumCPUs <- research_Element(FileInput_List, "NumCPUs")
+		  
+		  return (typeof(FileInput_List))
+		}
+	}
 }
+
+
+research_Element <- function(Input_file, Researched_element){
+	#IN : pre-treated file : \n replaced by space then space splited into list, researched element (string)
+	#OUT : 
+	out <- tryCatch
+		{
+			#research element StartTime
+			Pos_eli_time <- grep(Researched_element,Input_file)
+			Pos1 <- as.integer(strsplit(x = as.character(Pos_eli_time), split = " ")[[2]]) #position of list containing Start Time
+
+			#list containing research element
+			list <- Input_file[Pos1]
+
+			#position of element inside sub list
+			Pos2 <- as.integer(grep(Researched_element,list[[1]]))
+			Complet_Element <- Input_file[[Pos1]][Pos2]
+
+			Only_Element <- strsplit(x = Complet_Element, split = "=")[[1]][2]
+
+			return (Only_Element)
+		},
+		error = function(cond){
+
+		}
+	return (out)
+}
+
+#execution time
+
+#memory
+
+
+#function call with complete list of .o file
+result <- parsed_folder(o_files_list)
+print(result)
