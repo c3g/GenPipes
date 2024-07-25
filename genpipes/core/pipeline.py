a670c16e11afdc8b5b8d85ddc1998aac8c741acd
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2024-07-25 12:53:41 -0400

    add missing paragraph to help output

--- genpipes/core/pipeline.py
+++ genpipes/core/pipeline.py
@@ -182,7 +182,7 @@ class Pipeline(object):
         epilog = "\n".join(step_lst)
         detailed_steps = ""
         if '--help' in argv:
-            epilog = "THIS IS THE EPILOG" + epilog + "THIS IS AFTER THE EPILOG" + "\n".join(steps_doc)
+            epilog = epilog + "\n" + "\n".join(steps_doc)
             #detailed_steps = "\n".join(steps_doc)
         return epilog #, detailed_steps
 
