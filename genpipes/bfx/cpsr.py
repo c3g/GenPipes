5a791cd246915b9ce7f5c0a31cc697c6f7cb2087
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2024-08-19 11:03:49 -0400

    bfx : adjust expected cpsr outputs for v2

--- genpipes/bfx/cpsr.py
+++ genpipes/bfx/cpsr.py
@@ -77,7 +77,7 @@ def report2(
         ):
     assembly = global_conf.global_get(ini_section, 'assembly')
     output = [
-        os.path.join(output_dir, tumor_id + ".cpsr." + assembly + ".json.gz"),
+        os.path.join(output_dir, tumor_id + ".cpsr." + assembly + ".vcf.gz"),
         os.path.join(output_dir, tumor_id + ".cpsr." + assembly + ".html")
     ]
     # use tmp dir for cpsr to avoid disk quota issues caused by bcftools tmp dir settings
