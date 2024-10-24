c43375bb7a42eb27d1302abeabddaabc2e8bf055
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2024-10-24 17:03:47 -0400

    Consolidating container

--- genpipes/tools/get_wrapper.sh
+++ genpipes/tools/get_wrapper.sh
@@ -5,19 +5,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CONTAINER_DIR=$SCRIPT_DIR/../../resources/container
 mkdir "$CONTAINER_DIR"
 wget -c http://github.com/c3g/genpipes_in_a_container/releases/download/${GIAC_VERSION}/wrapper_genpipes.tgz -O - | tar -xz -C ${CONTAINER_DIR} --strip-components=1
-echo Installing singularity image and configuration file
+echo "Installing singularity image and configuration file in $CONTAINER_DIR"
 if test -f "${CONTAINER_DIR}/etc/wrapper.conf"; then
   rel=$(realpath --relative-to=$PWD ${CONTAINER_DIR}/etc/wrapper.conf)
-  echo "./$rel exist,"
-  read -r -p  "  do you want to overwrite it y[n]:?" response
-
+  read -r -p  "./$rel exist, do you want to overwrite it y[n]:?" response
    case "$response" in
      [yY][eE][sS]|[yY])
-          echo updating confing
+          echo "Updating config file."
           mv  "${CONTAINER_DIR}/etc/wrapper.conf.tpl" "${CONTAINER_DIR}/etc/wrapper.conf"
           ;;
      *)
-          echo keeping old config
+          echo "Keeping old config file."
           ;;
     esac
 else
