0802c4b1826e13dbac34e1cf6a8c3c2e005719be
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2024-11-12 14:18:34 -0500

    add scheduler option to log_report tool

--- genpipes/tools/tools.py
+++ genpipes/tools/tools.py
@@ -58,14 +58,14 @@ def add_subcommands(parser):
     # Create the parser for the "log_report" subcommand
     parser_log_report = tools_subparsers.add_parser('log_report', help='Generate the log report for a GenPipes run')
     parser_log_report.add_argument('job_list_path', help="Path to a GenPipes job list")
-    parser_log_report.add_argument('--remote', '-r', help="Remote HPC where the job was run", choices=['beluga', 'cedar', 'narval', 'abacus'], default=None)
-    # As using sys.argv to check remote value making sure it's only in the context of a "genpipes tools log_report call"
-    if any((remote in sys.argv for remote in ['--remote', '-r'])) and "tools" in sys.argv and "log_report" in sys.argv:
-        if 'abacus' in sys.argv:
+    parser_log_report.add_argument('--job_scheduler', '-j', help="HPC scheduler where the job was run", choices=['slurm', 'pbs'], default='slurm')
+    # As using sys.argv to check scheduler value making sure it's only in the context of a "genpipes tools log_report call"
+    if any((job_scheduler in sys.argv for job_scheduler in ['--job_scheduler', '-j'])) and "tools" in sys.argv and "log_report" in sys.argv:
+        if 'pbs' in sys.argv:
             parser_log_report.add_argument('--memtime', '-m', help="Output also memtime values if present in job output files", action='store_true', default=False)
             parser_log_report.add_argument('--success', '-s', help="Show successful jobs only", action='store_true', default=False)
             parser_log_report.add_argument('--nosuccess', '--nos', help="Show unsuccessful jobs only i.e. failed or uncompleted jobs", action='store_true', default=False)
-        elif any((cluster in sys.argv for cluster in ['beluga', 'cedar', 'narval'])):
+        elif 'slurm' in sys.argv:
             parser_log_report.add_argument('--loglevel', help="Standard Python log level. Default: ERROR", choices=['ERROR', 'WARNING', 'INFO', "CRITICAL"], default='ERROR')
             parser_log_report.add_argument('--tsv', help="Output to tsv file")
             parser_log_report.add_argument('--quiet', '-q', help="No report printed to terminal", action='store_true', default=False)
@@ -114,7 +114,7 @@ def run_log_report(args):
     """
     Run the log_report.pl script with the given arguments.
     """
-    if args.remote == 'abacus':
+    if args.job_scheduler == 'abacus':
         # Call log_report.pl
         log_report_pl = os.path.join(os.path.dirname(__file__), 'log_report.pl')
         cmd = [log_report_pl, args.job_list_path]
