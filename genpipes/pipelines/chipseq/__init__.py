2fc55588bfbed630960f7ba9b90419dbbafd0298
Gerardo Zapata Abogado <gerardo_za_94@hotmail.com>
2025-02-28 13:11:00 -0500

    ChIP32 - Correction for _init__.py and bfx -  list of inputs?

--- genpipes/pipelines/chipseq/__init__.py
+++ genpipes/pipelines/chipseq/__init__.py
@@ -871,7 +871,7 @@ done""".format(
         ## Loop to get bam files per sample
         for sample in self.samples:
 
-            ## if - to get cleaned or not bams / for each mark.type
+            ## if else - to get cleaned or not bams / for each mark.type
             if global_conf.global_get('bedtools_intersect', 'blacklist', required=False, param_type='filepath'):
                 all_file_list = [os.path.join(self.output_dirs['alignment_output_directory'], sample.name, mark_name, f"{sample.name}.{mark_name}.sorted.dup.filtered.cleaned.bam") for mark_name, mark_type in sample.marks.items()]
             else:
