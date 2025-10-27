d2a2d431ce627e6cfd1e332a80fda9c31075d6dc
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2025-10-27 10:49:43 -0400

    testing export of timestamp variable to prevent restarts

--- genpipes/pipelines/dnaseq/__init__.py
+++ genpipes/pipelines/dnaseq/__init__.py
@@ -2822,7 +2822,7 @@ END
 
                 if self.project_tracking_json:
                     samples = [sample]
-                    pcgr_output_file = os.path.join(self.output_dir, "job_output", "report_pcgr", f"{job_name}_$TIME_STAMP.o")
+                    pcgr_output_file = os.path.join(self.output_dir, "job_output", "report_pcgr", f"{job_name}_\$TIME_STAMP.o")
                     jobs.append(
                         concat_jobs(
                             [
