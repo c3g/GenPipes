8e8c1b80504021b165efd85f7911b0a38063e777
SoGa83 <soofiagarmeh@gmail.com>
2026-09-22 11:45:54 -0400

    methylseq: add conditional_ln symlink in gembs_call for .json name

--- genpipes/pipelines/methylseq/__init__.py
+++ genpipes/pipelines/methylseq/__init__.py
@@ -1261,8 +1261,12 @@ cat {metrics_all_file} | sed 's/%_/perc_/g' | sed 's/#_/num_/g' >> {ihec_multiqc
                                 sample.name,
                                 bam,
                                 output_prefix
-                                )
-                            ],
+                                ),
+                            bash.conditional_ln(
+                                target_file=output_prefix + "_call.json",
+                                link=output_prefix + ".json"
+                            )
+                        ],
                         name = "gembs_call." + sample.name,
                         samples = [sample],
                         input_dependency=[bam,gembs_config]
